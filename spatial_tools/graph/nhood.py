cb4cf8edc317b6f2fd7bcfc9320e8b7c4909c7a4
AnnaChristina <anna.schaar@tum.de>
2020-09-14 15:51:58 +0200

    numerical assortativity coefficient

--- spatial_tools/graph/nhood.py
+++ spatial_tools/graph/nhood.py
@@ -1,8 +1,7 @@
 """Functions for neighborhood enrichment analysis (permutation test, assortativity measures etc.)
 """
-
-import numpy as np
 import networkx as nx
+import warnings
 
 
 class AssortativityMeasures:
@@ -11,18 +10,59 @@ class AssortativityMeasures:
     """
     def get_networkx_graph(
             self,
-            a,
+            adjacency_matrix,
             node_attributes
     ):
         """
         Function to create networkx graph from adjacency matrix a with node attributes attr.
         Networkx graph is needed to calculate assortativity coefficients with networkx built-in functions.
-        :param a: adjacency matrix
+        :param adjacency_matrix: sparse adjacency matrix
         :param node_attributes: dict of nodes
-        :return: networkx graph G
+        :return: networkx graph G with node_attributes
         """
+        graph = nx.from_scipy_sparse_matrix(adjacency_matrix)
+        if node_attributes is not None:
+            nx.set_node_attributes(graph, node_attributes)
+        self.graph = graph
+        return self.graph
 
-        self.G = nx.from_scipy_sparse_matrix(a)
-        if attr is not None:
-            nx.set_node_attributes(G, node_attributes)
-        return self.G
+    def calculate_degree_assortativity_coefficient(
+            self,
+            graph
+    ):
+        """
+        Calculates degree assortativity coefficient for a graph G
+        :param graph: networkx graph
+        :return: degree assortativity coefficient r (int)
+        """
+        r = nx.degree_assortativity_coefficient(graph)
+        return r
+
+    def calculate_assortativity_coefficient(
+            self,
+            graph,
+            node_attributes: str
+    ):
+        """
+        Calculates degree assortativity coefficient for a graph G with node attributes node_attributes
+        :param graph: networkx graph
+        :param node_attributes: node attributes stored in networkx graph
+        :return: assortativity coefficient r (int)
+        """
+        attr = nx.get_node_attributes(graph, node_attributes)
+        if not attr:  # checking if graph has node_attributes
+            warnings.warn("The given node attribute is not given in the graph, choose different node attributes or "
+                          "connect the given attrbute to the graph.")
+
+        # ToDo: difference in calculation of numeric assortativity coefficient and categorical coefficient - check why
+        #  or implement version which uses the respective function
+        r = nx.attribute_assortativity_coefficient(graph, node_attributes)
+        return r
+
+    def _add_r_to_anndata_object(self):
+        """
+        Function that adds assortativity coefficient as unstructured annotation to AnnData object
+        :return:
+        """
+        # ToDo
+        raise NotImplementedError
