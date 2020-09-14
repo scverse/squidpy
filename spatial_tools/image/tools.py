6c57c409aeb1f8226cf4d2e0efddd60755142b69
Hannah Spitzer <hannah.spitzer@helmholtz-muenchen.de>
2020-09-14 12:54:39 +0200

    add skeleton function for image cropping

--- spatial_tools/image/tools.py
+++ spatial_tools/image/tools.py
@@ -1,1 +1,7 @@
 import numpy as np
+import imageio
+import os
+
+def read_tif(dataset_folder, dataset_name):
+    img = imageio.imread(os.path.join(dataset_folder, f"{dataset_name}_image.tif"))
+    return img
