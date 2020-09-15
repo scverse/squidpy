5daa1890582060546afa955c307c4b896081eec8
Gottfrid_91 <olleholmberg19@gmail.com>
2020-09-15 10:37:10 +0200

    adding timing function to image _utils.py

--- spatial_tools/image/_utils.py
+++ spatial_tools/image/_utils.py
@@ -1,1 +1,15 @@
 import numpy as np
+
+
+import functools import wraps
+from time import time
+
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print(f"func:{f.__name__} took: {te-ts} sec")
+        return result
+    return wrap
