b76dbd3ddb77d6809fe77657b4b854518be078cc
Hannah Spitzer <hannah.spitzer@helmholtz-muenchen.de>
2020-09-25 16:03:01 +0200

    fix docstrings and add test headers

--- spatial_tools/image/object.py
+++ spatial_tools/image/object.py
@@ -1,53 +1,88 @@
 import numpy as np
-from typing import Union, List
+from typing import Union, List, Optional
 import xarray as xr
 from ._utils import _num_pages 
-from .crop import crop_img
 
 class ImageContainer:
-    """
-    Container for in memory / tif images. Allows for lazy and chunked reading via rasterio and dask
+    """\
+    Container for in memory or on-disk tiff images. 
+    
+    Allows for lazy and chunked reading via rasterio and dask.
     An instance of this class is given to all image processing functions, along with an anndata instance
     if necessary.
+    
+    Attributes
+    ----------
+    data
+        Xarray dataset containing the image data
+    
+    Methods
+    -------
+    add_img(img, img_id)
+        Add layers from numpy / tiff file to `data` with key `img_id`.
+        
+    crop(x, y)
+        Crop image centered around coordinates (x,y) from `data`.
     """
     data: xr.Dataset
         
-    def __init__(self, img: Union[str, np.ndarray], img_id: Union[str, List[str]] = None, lazy: bool = True, chunks: int = None, ):
-        """
+    def __init__(self, img: Union[str, np.ndarray], img_id: Optional[Union[str, List[str]]] = None, 
+                 lazy: bool = True, chunks: Optional[int] = None):
+        """\
+        Set up ImageContainer from numpy array or on-disk tiff.
+        
         Processes image as in memory numpy array or uses xarrays rasterio reading functions to load from disk 
         (with caching) if image is a file path.
         If chunks are specified, the xarray is wrapped in a dask lazy dask array using the chunk size.
 
-        An instance of this class is given to all image processing functions, along with an anndata instance
-        if necessary.
-
-        :param img:
-        :param lazy: use rasterio/dask to lazily load image
-        :param chunks: chunk size for dask
-        :param img_id: name for img. For multi-page tiffs should be a list. 
-            If not specified, DataArrays will be named "image_{i}"
+        Params
+        ------
+        img
+            Numpy array or path to tiff file.
+        img_id
+            Key (name) to be used for img. For multi-page tiffs this should be a list.
+            If not specified, DataArrays will be named "image_{i}".
+        lazy
+            Use rasterio/dask to lazily load image.
+        chunks
+            Chunk size for dask.
         """
         if chunks is not None:
             chunks = {'x': chunks, 'y': chunks}
-        self.chunks = chunks
-        self.lazy = lazy
+        self._chunks = chunks
+        self._lazy = lazy
         self.data = xr.Dataset()
         if img is not None:
             self.add_img(img, img_id)
         
     @classmethod
-    def open(cls, fname: str, lazy: bool = True, chunks: int = None):
-        """
-        initialize using a previously saved netcdf file
+    def open(cls, fname: str, lazy: bool = True, chunks: Optional[int] = None):
+        """\
+        Initialize using a previously saved netcdf file.
+        
+        Params
+        ------
+        fname
+            Path to the saved .nc file.
+        lazy
+            Use dask to lazily load image.
+        chunks
+            Chunk size for dask.
         """
         self = cls(img=None, lazy=lazy, chunks=chunks)
-        self.data = xr.open_dataset(fname, chunks=self.chunks)
-        if not self.lazy:
+        self.data = xr.open_dataset(fname, chunks=self._chunks)
+        if not self._lazy:
             self.data.load()
         return self
     
     def save(self, fname: str):
-        """saves dataset as netcdf file"""
+        """Save dataset as netcdf file.
+        
+        Params
+        ------
+        fname
+            Path to the saved .nc file.
+        """
         self.data.to_netcdf(fname, mode='a')
         
         
@@ -56,9 +91,17 @@ class ImageContainer:
         Add layer(s) from numpy image / tiff file. 
         For numpy arrays, assume that dims are: channels, y, x
         
-        :param img:
-        :param img_id:
-        :return: None
+        Params
+        ------
+        img
+            Numpy array or path to tiff file.
+        img_id
+            Key (name) to be used for img. For multi-page tiffs this should be a list.
+            If not specified, DataArrays will be named "image_{i}".
+       
+        Returns
+        -------
+        None
         """
         imgs = self._load_img(img)
         if img_id is None:
@@ -76,16 +119,25 @@ class ImageContainer:
         # add to data
         for img, img_id in zip(imgs, img_ids):
             self.data[img_id] = img
-        if not self.lazy:
+        if not self._lazy:
             # load in memory
             self.data.load()
     
-    def _load_img(self, img: Union[str, np.ndarray]):
-        """
-        Load img as xarray. Supports numpy arrays and (multi-page) tiff files.
-        For numpy arrays, assume that dims are: channels, y, x
+    def _load_img(self, img: Union[str, np.ndarray]) -> List[xr.DataArray]:
+        """\
+        Load img as xarray. 
         
-        :returns: list of DataArrays
+        Supports numpy arrays and (multi-page) tiff files.
+        For numpy arrays, assume that dims are: `'channels, y, x'`
+        
+        Params
+        ------
+        img
+            Numpy array or path to tiff file.
+            
+        Returns
+        -------
+        List of DataArrays containing loaded images.
         """
         imgs = []
         if isinstance(img, np.ndarray):
@@ -99,29 +151,44 @@ class ImageContainer:
             num_pages = _num_pages(img)
             # read all pages using rasterio
             for i in range(1, num_pages+1):
-                data = xr.open_rasterio(f"GTIFF_DIR:{i}:{img}", chunks=self.chunks, parse_coordinates=False)
+                data = xr.open_rasterio(f"GTIFF_DIR:{i}:{img}", chunks=self._chunks, parse_coordinates=False)
                 data = data.rename({'band': 'channels'})
                 imgs.append(data)
         else:
             raise ValueError(img)
         return imgs
 
-    def crop(self, x, y, s=100, img_id: Union[str, List[str]] = None, **kwargs):
-        """
-        extract a centered at `x` and `y`. 
-        Attrs:
-            img_ids (list): list of images that should be used to obtain the crop
-            x (int): x coord of crop in `img`
-            y (int): y coord of crop in `img`
-            s (int): width and heigh of the crop in pixels
-            scale (float): resolution of the crop (smaller -> smaller image)
-            mask_circle (bool): mask crop to a circle
-            cval (float): the value outside image boundaries or the mask
-            dtype (str): optional, type to which the output should be (safely cast)
+    def crop(self, x: int, y: int, s: int = 100, img_id: Optional[Union[str, List[str]]] = None, **kwargs) -> np.ndarray:
+        """\
+        Extract a crop centered at `x` and `y`. 
+        
+        Params
+        ------
+        x: int
+            X coord of crop (in pixel space).
+        y: int
+            Y coord of crop (in pixel space).
+        s: int
+            Width and heigh of the crop in pixels.
+        scale: float
+            Default is 1.0.
+            Resolution of the crop (smaller -> smaller image).
+        mask_circle: bool
+            Default is False.
+            Mask crop to a circle.
+        cval: float
+            Default is 0
+            The value outside image boundaries or the mask.
+        dtype: str
+            Optional, type to which the output should be (safely) cast. 
+            Currently supported dtypes: 'uint8'.
             
-        Returns:
-            np.ndarray with dimentions: y, x, channels (concatenated over all images)
+        Returns
+        -------
+        np.ndarray with dimentions: y, x, channels (concatenated over all images)
         """
+        from .crop import crop_img
+        
         if img_id is None:
             img_ids = list(self.data.keys())
         elif isinstance(img_id, str):
