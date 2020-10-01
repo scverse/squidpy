99b6dd80a56e2bb5d298a0f5d21df70a3dbc0c5b
Sergei Rybakov <koncopd@gmail.com>
2020-10-01 18:19:26 +0200

    Build graph fix and cleanup (#69)
    * fix and clean spatial_connectivity
    
    * add test templates
    
    * correct type

---
+++ spatial_tools/tests/test_spatial_connectivity.py
@@ -0,0 +1,16 @@
+import pytest
+from spatial_tools.graph import spatial_connectivity
+
+
+def test_spatial_connectivity_visium():
+    """
+    check correctness of neighborhoods for visium coordinates
+    """
+    pass
+
+
+def test_spatial_connectivity_non_visium():
+    """
+    check correctness of neighborhoods for non-visium coordinates
+    """
+    pass
