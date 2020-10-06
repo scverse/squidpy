e6a40213b006f6e4865fd66ff9b06c93d0f9d30a
Anna Schaar <44966624+AnnaChristina@users.noreply.github.com>
2020-10-06 15:01:36 +0200

    unit tests centrality scores in nhood  (#74)
    * clean up centrality scores and write plotting module
    
    * remove data from local tests
    
    * write draft for tests
    
    * create common plotting class for both graph and image
    
    * write unit tests for centrality scores and cluster interactions
    
    * remove plotting from init in graph

--- spatial_tools/graph/__init__.py
+++ spatial_tools/graph/__init__.py
@@ -3,4 +3,3 @@ from .build import spatial_connectivity
 from .nhood import permutation_test_leiden_pairs, cluster_centrality_scores, cluster_interactions
 from .clustering import prepare_data_for_clustering, compute_louvain_on_joined_connectivities, plot_louvain_on_graph, \
     plot_louvain_on_umap
-from .plotting import plot_cluster_centrality_scores, plot_cluster_interactions
