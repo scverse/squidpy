d938b8a0b6681c4f0b39c6cde3cbfde3b34a9e40
Hannah Spitzer <hannah.spitzer@helmholtz-muenchen.de>
2020-10-14 18:11:15 +0200

    enable reading jpegs, add test for crop_generator

--- spatial_tools/image/object.py
+++ spatial_tools/image/object.py
@@ -1,13 +1,15 @@
 import numpy as np
 from typing import Union, List, Optional
 import xarray as xr
-from ._utils import _num_pages 
+from ._utils import _num_pages
+from imageio import imread
+
 
 class ImageContainer:
     """\
-    Container for in memory or on-disk tiff images. 
+    Container for in memory or on-disk tiff or jpg images. 
     
-    Allows for lazy and chunked reading via rasterio and dask.
+    Allows for lazy and chunked reading via rasterio and dask (if input is a tiff image).
     An instance of this class is given to all image processing functions, along with an anndata instance
     if necessary.
     
@@ -19,17 +21,23 @@ class ImageContainer:
     Methods
     -------
     add_img(img, img_id)
-        Add layers from numpy / tiff file to `data` with key `img_id`.
+        Add layers from numpy / image file to `data` with key `img_id`.
         
     crop(x, y)
         Crop image centered around coordinates (x,y) from `data`.
     """
+
     data: xr.Dataset
-        
-    def __init__(self, img: Union[str, np.ndarray], img_id: Optional[Union[str, List[str]]] = None, 
-                 lazy: bool = True, chunks: Optional[int] = None):
+
+    def __init__(
+        self,
+        img: Union[str, np.ndarray],
+        img_id: Optional[Union[str, List[str]]] = None,
+        lazy: bool = True,
+        chunks: Optional[int] = None,
+    ):
         """\
-        Set up ImageContainer from numpy array or on-disk tiff.
+        Set up ImageContainer from numpy array or on-disk tiff / jpg.
         
         Processes image as in memory numpy array or uses xarrays rasterio reading functions to load from disk 
         (with caching) if image is a file path.
@@ -48,13 +56,13 @@ class ImageContainer:
             Chunk size for dask.
         """
         if chunks is not None:
-            chunks = {'x': chunks, 'y': chunks}
+            chunks = {"x": chunks, "y": chunks}
         self._chunks = chunks
         self._lazy = lazy
         self.data = xr.Dataset()
         if img is not None:
             self.add_img(img, img_id)
-        
+
     @classmethod
     def open(cls, fname: str, lazy: bool = True, chunks: Optional[int] = None):
         """\
@@ -74,37 +82,38 @@ class ImageContainer:
         if not self._lazy:
             self.data.load()
         return self
-    
+
     def save(self, fname: str):
         """Save dataset as netcdf file.
-        
+
         Params
         ------
         fname
             Path to the saved .nc file.
         """
-        self.data.to_netcdf(fname, mode='a')
-        
-        
-    def add_img(self, img: Union[str, np.ndarray], img_id: Union[str, List[str]] = None):
+        self.data.to_netcdf(fname, mode="a")
+
+    def add_img(
+        self, img: Union[str, np.ndarray], img_id: Union[str, List[str]] = None
+    ):
         """
-        Add layer(s) from numpy image / tiff file. 
+        Add layer(s) from numpy image / tiff or jpg file.
         For numpy arrays, assume that dims are: channels, y, x
-        
-        The added image has to have the same number of channels as the original image, or no channels. 
-        
+
+        The added image has to have the same number of channels as the original image, or no channels.
+
         Params
         ------
         img
-            Numpy array or path to tiff file.
+            Numpy array or path to image file.
         img_id
             Key (name) to be used for img. For multi-page tiffs this should be a list.
             If not specified, DataArrays will be named "image_{i}".
-       
+
         Returns
         -------
         None
-        
+
         Raises
         ------
         ValueError
@@ -112,7 +121,7 @@ class ImageContainer:
         """
         imgs = self._load_img(img)
         if img_id is None:
-            img_id = 'image'
+            img_id = "image"
         if isinstance(img_id, str):
             if len(imgs) > 1:
                 img_ids = [f"{img_id}_{i}" for i in range(len(imgs))]
@@ -122,25 +131,30 @@ class ImageContainer:
             img_ids = img_id
         else:
             raise ValueError(img_id)
-        assert len(img_ids) == len(imgs), f"Have {len(imgs)} images, but {len(img_ids)} image ids"
+        assert len(img_ids) == len(
+            imgs
+        ), f"Have {len(imgs)} images, but {len(img_ids)} image ids"
         # add to data
         for img, img_id in zip(imgs, img_ids):
             self.data[img_id] = img
         if not self._lazy:
             # load in memory
             self.data.load()
-    
+
     def _load_img(self, img: Union[str, np.ndarray]) -> List[xr.DataArray]:
         """\
         Load img as xarray. 
         
-        Supports numpy arrays and (multi-page) tiff files.
+        Supports numpy arrays and (multi-page) tiff files, and jpg files
         For numpy arrays, assume that dims are: `'channels, y, x'`
+        
+        NOTE: lazy loading via dask is currently not supported for on-disk jpg files. 
+        They will be loaded in memory.
         
         Params
         ------
         img
-            Numpy array or path to tiff file.
+            Numpy array or path to image file.
             
         Returns
         -------
@@ -154,25 +168,49 @@ class ImageContainer:
         imgs = []
         if isinstance(img, np.ndarray):
             if len(img.shape) > 3:
-                raise ValueError(f"img has more than 3 dimensions. img.shape is {img.shape}")
-            dims = ['channels', 'y', 'x']
+                raise ValueError(
+                    f"img has more than 3 dimensions. img.shape is {img.shape}"
+                )
+            dims = ["channels", "y", "x"]
             if len(img.shape) == 2:
-                dims = ['y', 'x']
+                dims = ["y", "x"]
             xr_img = xr.DataArray(img, dims=dims)
             imgs.append(xr_img)
         elif isinstance(img, str):
-            # get the number of pages in the file
-            num_pages = _num_pages(img)
-            # read all pages using rasterio
-            for i in range(1, num_pages+1):
-                data = xr.open_rasterio(f"GTIFF_DIR:{i}:{img}", chunks=self._chunks, parse_coordinates=False)
-                data = data.rename({'band': 'channels'})
-                imgs.append(data)
+            ext = img.split(".")[-1]
+            if ext in ("tif", "tiff"):
+                # get the number of pages in the file
+                num_pages = _num_pages(img)
+                # read all pages using rasterio
+                for i in range(1, num_pages + 1):
+                    data = xr.open_rasterio(
+                        f"GTIFF_DIR:{i}:{img}",
+                        chunks=self._chunks,
+                        parse_coordinates=False,
+                    )
+                    data = data.rename({"band": "channels"})
+                    imgs.append(data)
+            elif ext in ("jpg", "jpeg"):
+                img = imread(img)
+                # jpeg has channels as last dim - transpose
+                img = img.transpose(2, 0, 1)
+                dims = ["channels", "y", "x"]
+                xr_img = xr.DataArray(img, dims=dims)
+                imgs.append(xr_img)
+            else:
+                raise NotImplementedError(f"Files with extension {ext}")
         else:
             raise ValueError(img)
         return imgs
 
-    def crop(self, x: int, y: int, s: int = 100, img_id: Optional[Union[str, List[str]]] = None, **kwargs) -> np.ndarray:
+    def crop(
+        self,
+        x: int,
+        y: int,
+        s: int = 100,
+        img_id: Optional[Union[str, List[str]]] = None,
+        **kwargs,
+    ) -> np.ndarray:
         """\
         Extract a crop centered at `x` and `y`. 
         
@@ -202,17 +240,16 @@ class ImageContainer:
         np.ndarray with dimentions: y, x, channels (concatenated over all images)
         """
         from .crop import crop_img
-        
+
         if img_id is None:
             img_ids = list(self.data.keys())
         elif isinstance(img_id, str):
             img_ids = [img_id]
         else:
             img_ids = img_id
-        
+
         crops = []
         for img_id in img_ids:
             img = self.data[img_id]
             crops.append(crop_img(img, x, y, s, **kwargs))
         return np.concatenate(crops, axis=-1)
-    
--- spatial_tools/tests/test_image_crop.py
+++ spatial_tools/tests/test_image_crop.py
@@ -1,23 +1,45 @@
 import pytest
+import numpy as np
+import os
+import anndata as ad
+from spatial_tools.image.crop import crop_generator
+from spatial_tools.image.object import ImageContainer
+
 
 def test_crop_generator():
-    """
+    """\
     for simulated adata + image, generate crops. 
     Make sure that the correct amount of crops are generated
-    and that the crops have the correct content
     
-    TODO
+    TODO: test that the crops have the correct content - how?
     """
-    pass
+    # load test data
+    adata = ad.read(os.path.join(os.path.dirname(__file__), "_data/test_data.h5ad"))
+    cont = ImageContainer(os.path.join(os.path.dirname(__file__), "_data/test_img.jpg"))
+
+    i = 0
+    expected_size = np.round(
+        adata.uns["spatial"]["V1_Adult_Mouse_Brain"]["scalefactors"][
+            "spot_diameter_fullres"
+        ]
+    )
+    for obs_id, crop in crop_generator(adata, cont):
+        # crops have expected size?
+        assert crop.shape[0] == expected_size
+        i += 1
+    # expected number of crops are generated?
+    assert i == adata.obsm["spatial"].shape[0]
+
 
 def test_uncrop_img():
-    #TODO David
+    # TODO David
     pass
-    
+
+
 def test_crop_img():
     """
     crop different sizes and scales. Check padding + correct crop location
-    
+
     TODO currently done in test_image_objecct.test_crop
     """
     pass
