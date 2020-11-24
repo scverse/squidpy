2bc8edb3ef57f7a205c0056f6e1a98bc54179079
giovp <giov.pll@gmail.com>
2020-11-24 18:38:56 +0100

    add colors and layers

--- squidpy/plotting/_image.py
+++ squidpy/plotting/_image.py
@@ -1,13 +1,25 @@
+from typing import Union, Optional, Sequence
+
 from anndata import AnnData
 
 import numpy as np
+from scipy.sparse import csr_matrix
 
 import matplotlib.pyplot as plt
+from matplotlib.colors import to_hex
 
 from ..image.object import ImageContainer  # noqa: F401
 
 
-def interactive(adata: AnnData, img: np.ndarray, *args):
+def interactive(
+    adata: AnnData,
+    img: np.ndarray,
+    color: Union[str, Sequence[str], None] = None,
+    n_polygon: Union[int, Sequence[str]] = 6,
+    with_qt: Optional[bool] = False,
+    color_map: str = "viridis",
+    palette: str = "Dark2",
+):
     """
     Launch Napari for spot visualization.
 
@@ -20,31 +32,84 @@ def interactive(adata: AnnData, img: np.ndarray, *args):
     try:
         import napari
     except ImportError:
-        raise ImportError("\nplease install napari: \n\n" "\tpip install napari\n")
+        raise ImportError("please install napari: pip install 'napari[all]'")
 
-    # colors_key = f"{cluster_key}_colors"
-    # if colors_key in adata.uns.keys():
-    #     ad.uns[colors_key] = adata.uns[colors_key]
+    color = [color] if isinstance(color, str) or color is None else list(color)
+    assert set(color).issubset(
+        adata.obs_keys() + adata.var_names.values.tolist()
+    ), "color is not a subset of `adata.obs_keys()+adata.var_names`"
 
-    spot_diameter = adata.uns["spatial"]["V1_Adult_Mouse_Brain"]["scalefactors"]["spot_diameter_fullres"]
+    library_id = list(adata.uns["spatial"].keys())
+    spot_diameter = adata.uns["spatial"][library_id[0]]["scalefactors"]["spot_diameter_fullres"]
 
-    new_shape = _create_shape(adata.obsm["spatial"], spot_diameter * 0.5)
+    color_lst = _get_colors(adata, color, color_map, palette)
 
-    viewer = napari.view_image(img, rgb=True)
-    layer = viewer.add_shapes(  # noqa: F841
-        new_shape,
-        shape_type="polygon",
-        edge_width=1,
-        edge_color="white",
-        face_color=None,
-    )
+    shapes = _create_shape(adata.obsm["spatial"], lt=spot_diameter * 0.5, n=n_polygon)
+    shape_lst = [shapes] * len(color_lst)
+
+    name_lst = color
+
+    if with_qt:
+        with napari.gui_qt():
+            viewer = _open_napari(img, shape_lst, color_lst, name_lst)
+    else:
+        viewer = _open_napari(img, shape_lst, color_lst, name_lst)
 
     return viewer
 
 
+def _open_napari(img: np.ndarray, shapes: list, colors: list, names: list):
+
+    try:
+        import napari
+    except ImportError:
+        raise ImportError("please install napari: pip install 'napari[all]'")
+
+    viewer = napari.view_image(img, rgb=True)
+    for i in range(len(names)):
+        layer = viewer.add_shapes(  # noqa: F841
+            shapes[i], shape_type="polygon", edge_width=1, edge_color="black", face_color=colors[i], name=names[i]
+        )
+
+    return viewer
+
+
+def _get_colors(adata: AnnData, colors: list, color_map: str, palette: str):
+
+    color_lst = []
+    for c in colors:
+
+        if c in adata.obs_keys():
+            colors_key = f"{c}_colors"
+            if colors_key in adata.uns.keys():
+                cols = adata.uns[colors_key]
+            else:
+                ncat = adata.obs[c].unique().shape[0]
+                cmap = plt.get_cmap(palette, ncat)
+                cols = cmap(np.arange(ncat))
+
+            cols_dict = {k: v for v, k in zip(cols, adata.obs[c].cat.categories)}
+            cols = np.array([cols_dict[k] for k in adata.obs[c]])
+
+        elif c in adata.var_names.values.tolist():
+            if isinstance(adata.X, csr_matrix):
+                gexp = adata[:, c].X.todense().flatten()
+            else:
+                gexp = adata[:, c].X.flatten()
+
+            cmap = plt.get_cmap(color_map)
+            gexp = (gexp - gexp.min()) / (gexp.max() - gexp.min())
+            cols = cmap(gexp)
+            cols = [to_hex(i, keep_alpha=False) for i in cols[-1]]
+
+        color_lst.append(cols)
+
+    return color_lst
+
+
 def _create_shape(arr: np.ndarray, lt: float, n: int):
-    napari_coord = [np.array(_get_poly(x, y, lt, n)) for y, x in zip(arr[:, 0], arr[:, 1])]
-    return napari_coord
+    coord = [np.array(_get_poly(x, y, lt, n)) for y, x in zip(arr[:, 0], arr[:, 1])]
+    return coord
 
 
 def _get_poly(x, y, lt, n):
