4e445a9b3c8a7445e99b0c25eb7965a472b3e40c
giovp <giov.pll@gmail.com>
2020-11-24 22:42:39 +0100

    fix test

--- tests/tests_plotting/test_image.py
+++ tests/tests_plotting/test_image.py
@@ -1,18 +1,23 @@
+from pathlib import Path
+
 from anndata import AnnData
 
 from squidpy import plotting
 from squidpy.image.object import ImageContainer
 
+HERE: Path = Path(__file__).parents[1]
+IMG_PATH = HERE / "_data/test_img.jpg"
+
 
 def test_napari(adata: AnnData):
 
-    img_path = "./../_data/test_data.h5ad"
-    cont = ImageContainer(img_path)
+    library_id = list(adata.uns["spatial"].keys())
+    cont = ImageContainer(str(IMG_PATH), img_id=library_id[0])
 
     gene = adata.var_names[1]
     cluster = "leiden"
 
-    viewer = plotting.interactive(adata, cont, [cluster, gene])
+    viewer = plotting.interactive(adata, cont, [cluster, gene], with_qt=True)
 
     assert len(viewer.layers[cluster].data) == adata.shape[0]
     assert viewer.layers[cluster].data[0].shape == (6, 2)
