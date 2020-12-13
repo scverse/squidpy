1936ab126f3a7f5fe137b6a5f55a50c9207e2930
m0hammadl <mohammad.lotfollahi@helmholtz-muenchen.de>
2020-12-13 14:42:59 +0100

    edit init

--- squidpy/gr/__init__.py
+++ squidpy/gr/__init__.py
@@ -2,4 +2,4 @@
 from .build import spatial_connectivity
 from .nhood import nhood_enrichment, centrality_scores, interaction_matrix
 from ._ligrec import ligrec
-from .ppatterns import moran, ripley_k
+from .ppatterns import moran, ripley_k, neighborhood_plot
