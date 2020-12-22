d1db75a243ad02c4945027dce108a1d306569ffb
Hannah Spitzer <hannah.spitzer@helmholtz-muenchen.de>
2020-12-22 14:38:48 +0100

    fix ci

--- tests/tests_image/test_processing.py
+++ tests/tests_image/test_processing.py
@@ -6,7 +6,7 @@ from squidpy.im.object import ImageContainer
 from squidpy.im.processing import process_img
 
 
-@pytest.mark.parametrize("xs, ys", [(None, None), (40, 40)])
+@pytest.mark.parametrize(("xs", "ys"), [(None, None), (40, 40)])
 @pytest.mark.parametrize("processing", ["smooth", "gray"])
 def test_img_processing(xs, ys, processing):
     """Test skimage processing."""
