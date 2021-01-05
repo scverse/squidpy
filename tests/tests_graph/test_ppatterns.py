47958f7d1f87252114b6cd71745007b52e483ad1
giovp <giov.pll@gmail.com>
2021-01-05 22:15:17 +0100

    add tests

--- tests/tests_graph/test_ppatterns.py
+++ tests/tests_graph/test_ppatterns.py
@@ -2,7 +2,7 @@ from anndata import AnnData
 
 import numpy as np
 
-from squidpy.gr import moran, ripley_k
+from squidpy.gr import moran, ripley_k, co_occurrence
 
 
 # dummy_adata is now in conftest.py
@@ -29,3 +29,21 @@ def test_moran(dummy_adata: AnnData):
 
     # assert fdr correction in adata.uns
     assert "pval_sim_fdr_bh" in dummy_adata.var.columns
+
+
+def test_co_occurrence(dummy_adata: AnnData):
+    """
+    check ripley score and shape
+    """
+    co_occurrence(dummy_adata, cluster_key="cluster")
+
+    # assert occurrence in adata.uns
+    assert "cluster_co_occurrence" in dummy_adata.uns.keys()
+    assert "occ" in dummy_adata.uns["cluster_co_occurrence"].keys()
+    assert "interval" in dummy_adata.uns["cluster_co_occurrence"].keys()
+
+    # assert shapes
+    arr = dummy_adata.uns["cluster_co_occurrence"]["occ"]
+    assert arr.ndim == 3
+    assert arr.shape[2] == 49
+    assert arr.shape[1] == arr.shape[0] == dummy_adata.obs["cluster"].unique().shape[0]
