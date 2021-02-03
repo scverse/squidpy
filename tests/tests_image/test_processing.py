600fa9a5631ce1787ccc1e2504c784c24d857831
michalk8 <michal.klein@protonmail.com>
2021-02-03 23:29:30 +0100

    Fix processing tests

--- tests/tests_image/test_processing.py
+++ tests/tests_image/test_processing.py
@@ -6,19 +6,18 @@ from squidpy.im._process import process
 from squidpy.im._container import ImageContainer
 
 
-@pytest.mark.parametrize(("xs", "ys"), [(None, None), (40, 40)])
+@pytest.mark.parametrize("size", [(None, None), (40, 40)])
 @pytest.mark.parametrize("processing", ["smooth", "gray"])
-def test_img_processing(xs, ys, processing):
+def test_img_processing(size, processing):
     """Test skimage processing."""
     img_orig = np.zeros((100, 100, 3), dtype=np.uint8)
 
     cont = ImageContainer(img_orig, img_id="image_0")
     process(
         img=cont,
+        size=size,
         processing=processing,
         img_id="image_0",
-        xs=xs,
-        ys=ys,
         key_added="processed",
     )
     if processing == "smooth":
