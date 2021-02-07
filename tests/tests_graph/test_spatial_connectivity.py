5c72eaf2b1aa0470335f8cfa330d95240e73ba08
giovp <giov.pll@gmail.com>
2021-02-07 20:47:41 +0100

    closes #257

--- tests/tests_graph/test_spatial_connectivity.py
+++ tests/tests_graph/test_spatial_connectivity.py
@@ -1,3 +1,5 @@
+import pytest
+
 from anndata import AnnData
 
 import numpy as np
@@ -7,13 +9,16 @@ from squidpy._constants._pkg_constants import Key
 
 
 # todo add edge cases
-def test_spatial_neighbors_visium(visium_adata: AnnData):
+@pytest.mark.parametrize(("n_rings", "n_neigh", "sum_dist"), [(1, 6, 0), (2, 18, 30), (3, 36, 84)])
+def test_spatial_neighbors_visium(visium_adata: AnnData, n_rings: int, n_neigh: int, sum_dist: int):
     """
     check correctness of neighborhoods for visium coordinates
     """
-    for i, n_neigh in enumerate((6, 18, 36)):
-        spatial_neighbors(visium_adata, n_rings=i + 1)
-        assert visium_adata.obsp[Key.obsp.spatial_conn()][0].sum() == n_neigh
+    spatial_neighbors(visium_adata, n_rings=n_rings)
+    assert visium_adata.obsp[Key.obsp.spatial_conn()][0].sum() == n_neigh
+    assert visium_adata.uns[Key.uns.spatial_neighs()]["distances_key"] == Key.obsp.spatial_dist()
+    if n_rings > 1:
+        assert visium_adata.obsp[Key.obsp.spatial_dist()][0].sum() == sum_dist
 
 
 def test_spatial_neighbors_non_visium(non_visium_adata: AnnData):
