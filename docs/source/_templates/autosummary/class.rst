f92e54a9d8598199aa90612a646de11adca4d140
michalk8 <michal.klein@protonmail.com>
2021-02-07 22:41:04 +0100

    Unify nbsphinx/sphinx-gallery style, use hori logo

--- docs/source/_templates/autosummary/class.rst
+++ docs/source/_templates/autosummary/class.rst
@@ -35,5 +35,5 @@
    .. _sphx_glr_backref_{{fullname}}:
 
    .. minigallery:: {{fullname}}
-       :add-heading: Examples
+       :add-heading: Gallery
        :heading-level: -
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -132,7 +132,7 @@ spelling_filters = ["enchant.tokenize.URLFilter", "enchant.tokenize.EmailFilter"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
-html_logo = "_static/img/squidpy_vertical.png"
+html_logo = "_static/img/squidpy_horizontal.png"
 html_theme_options = {"navigation_depth": 4, "logo_only": True}
 html_show_sphinx = False
 
@@ -148,4 +148,5 @@ def setup(app: Sphinx) -> None:
     app.add_directive("minigallery", MaybeMiniGallery)
     app.add_css_file("css/custom.css")
     app.add_css_file("css/sphinx_gallery.css")
+    app.add_css_file("css/nbsphinx.css")
     app.add_css_file("css/dataframe.css")  # had to add this manually
