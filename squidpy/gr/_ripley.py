d5be9bb83f79919e295fb681e4d6184dc00dac86
giovp <giov.pll@gmail.com>
2021-05-06 20:57:13 +0200

    add ripley f

--- squidpy/gr/_ripley.py
+++ squidpy/gr/_ripley.py
@@ -1,12 +1,13 @@
 """Functions for point patterns spatial statistics."""
-from typing import Optional
+from typing import Tuple, Optional
 
 from numpy.random import default_rng
 from scipy.spatial import Delaunay, ConvexHull
+from sklearn.neighbors import NearestNeighbors
 import numpy as np
 
 
-def _ppp(hull: ConvexHull, n_simulations: int, n_observations: int, seed: Optional[int] = None) -> np.ndarray:
+def _ppp(coords: np.ndarray, n_simulations: int, n_observations: int, seed: Optional[int] = None) -> np.ndarray:
     """
     Simulate Poisson POint Process on a polygon.
 
@@ -26,6 +27,7 @@ def _ppp(hull: ConvexHull, n_simulations: int, n_observations: int, seed: Option
         An Array with shape (n_simulation, n_observations, 2).
     """
     rng = default_rng(None if seed is None else seed)
+    hull = ConvexHull(coords)
     vxs = hull.points[hull.vertices]
     deln = Delaunay(vxs)
 
@@ -44,3 +46,37 @@ def _ppp(hull: ConvexHull, n_simulations: int, n_observations: int, seed: Option
                 i_obs += 1
 
     return result.squeeze()
+
+
+def _build_support(coords: np.ndarray, n_steps: int) -> np.ndarray:
+    # TODO: add kdtree.max() option?
+    y_min = int(coords[:, 1].min())
+    y_max = int(coords[:, 1].max())
+    x_min = int(coords[:, 0].min())
+    x_max = int(coords[:, 0].max())
+    area = int((x_max - x_min) * (y_max - y_min))
+    support = np.linspace(0, (area / 2) ** 0.5, n_steps)
+
+    return support
+
+
+def _ripley_f(
+    coordinates: np.ndarray,
+    metric: str = "euclidean",
+    n_neighbors: int = 6,
+    n_simulations: int = 100,
+    n_observations: int = 1000,
+    n_steps: int = 50,
+    seed: Optional[int] = None,
+) -> Tuple[np.ndarray, np.ndarray]:
+
+    tree = NearestNeighbors(coordinates, metric=metric, n_neighbors=n_neighbors).fit(coordinates)
+    support = _build_support(coordinates, n_steps)
+    random = _ppp(coordinates, n_simulations, n_observations, seed=seed)
+    distances, _ = tree.kneighbors(random, k=1)
+    distances = distances.squeeze()
+
+    counts, bins = np.histogram(distances, bins=support)
+    fracs = np.cumsum(counts) / counts.sum()
+
+    return bins, np.asarray([0, *fracs])
