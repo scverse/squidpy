f506219d799832b3f102f1ac4e62bed1323b488e
giovp <giov.pll@gmail.com>
2021-12-10 21:31:56 +0100

    init spatial pl

---
+++ squidpy/pl/_spatial.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+from typing import Tuple, Mapping, Optional, Sequence
+
+from anndata import AnnData
+
+import numpy as np
+
+from squidpy._utils import NDArrayA
+from squidpy._constants._pkg_constants import Key
+
+# @d.dedent
+# def spatial(
+#     adata: AnnData,
+#     spatial_key: str = Key.obsm.spatial,
+#     color: Union[Sequence, str] = None,
+#     batch: str = None,
+#     cmap: str = "viridis",
+#     legend_kwargs: Mapping = None,
+#     label_kwargs: Mapping = None,
+# ):
+#     return
+
+
+def _get_spatial_attrs(
+    adata: AnnData,
+    spatial_key: str = Key.obsm.spatial,
+    library_id: Optional[Sequence[str] | None] = None,
+    img: Optional[NDArrayA | Sequence[NDArrayA]] = None,
+    img_key: str | None = None,
+    scale_factor: Optional[Sequence[float] | float] = None,
+    bw: bool = False,
+) -> Tuple[Sequence[str], Sequence[float], Sequence[NDArrayA]]:
+    """Return lists of image attributes saved in AnnData for plotting."""
+    library_id = Key.uns.library_id(adata, spatial_key, library_id, return_all=True)
+    if library_id is None:
+        raise ValueError("Should have prevented this upstream")
+
+    image_mapping = Key.uns.library_mapping(adata, spatial_key, Key.uns.image_key, library_id)
+    scalefactor_mapping = Key.uns.library_mapping(adata, spatial_key, Key.uns.scalefactor_key, library_id)
+
+    if not (image_mapping.keys() == scalefactor_mapping.keys()):  # check that keys match
+        raise KeyError(
+            f"Image keys: `{image_mapping.keys()}` and scalefactor keys: `{scalefactor_mapping.keys()}` are not equal."
+        )
+
+    if img_key is None:
+        img_key = _get_unique_map(image_mapping)  # get intersection of image_mapping.values()
+        img_key = next(iter(img_key))  # get first of set
+    else:
+        if img_key not in image_mapping.values():
+            raise ValueError(f"Image key: `{img_key}` does not exist. Available image keys: `{image_mapping.values()}`")
+
+    if scale_factor is None:  # get intersection of scale_factor and match to img_key
+        scale_factor = _get_unique_map(scalefactor_mapping)
+        scale_factor = [i for i in scale_factor if img_key in i][0]
+        if len(scale_factor) == 0:
+            raise ValueError(f"No `scale_factor` found that could match `img_key`: {img_key}.")
+        scale_factor = [adata.uns[Key.uns.spatial][i][Key.uns.scalefactor_key][scale_factor] for i in library_id]
+    elif isinstance(scale_factor, float):
+        scale_factor = [scale_factor] * len(library_id)
+    else:
+        if len(scale_factor) != len(library_id):
+            raise ValueError(
+                f"Len of scale_factor list: {len(scale_factor)} is not equal to len of library_id: {len(library_id)}."
+            )
+
+    if img is None:
+        img = [adata.uns[Key.uns.spatial][i][Key.uns.image_key][img_key] for i in library_id]
+    else:
+        if isinstance(img, np.ndarray) and (len(library_id) == 1):
+            img = [img]
+        elif isinstance(img, Sequence) and (len(library_id) != len(img)):
+            raise ValueError(f"Len of img list: {len(img)} is not equal to len of library_id: {len(library_id)}.")
+
+    if bw:
+        img = [np.dot(im[..., :3], [0.2989, 0.5870, 0.1140]) for im in img]
+
+    return library_id, scale_factor, img
+
+
+def _get_unique_map(dic: Mapping[str, Sequence[str]]) -> Sequence[str]:
+    return sorted(set.intersection(*map(set, dic.values())))
+
+
+# type: ignore
