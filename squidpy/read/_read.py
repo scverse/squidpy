8534d1f0c3351c52a059ae5a7e7c660af892cbc9
giovp <giov.pll@gmail.com>
2022-03-11 19:06:14 +0100

    improvements and change types

---
+++ squidpy/read/_read.py
@@ -0,0 +1,127 @@
+from __future__ import annotations
+
+from pathlib import Path
+import json
+
+from anndata import AnnData
+
+from squidpy.read._utils import _read_count, _read_coords, _read_images
+from squidpy._constants._pkg_constants import Key
+
+
+def read_visium(
+    path: str | Path,
+    genome: str | None = None,
+    *,
+    count_file: str = "filtered_feature_bc_matrix.h5",
+    library_id: str | None = None,
+    load_images: bool | None = True,
+    source_image_path: str | Path | None = None,
+) -> AnnData:
+    r"""
+    Read 10x-Genomics-formatted visium dataset.
+
+    In addition to reading regular 10x output,
+    this looks for the `spatial` folder and loads images,
+    coordinates and scale factors.
+    Based on the `Space Ranger output docs`_.
+    See :func:`~scanpy.pl.spatial` for a compatible plotting function.
+    .. _Space Ranger output docs:
+    https://support.10xgenomics.com/spatial-gene-expression/software/pipelines/latest/output/overview
+
+    Parameters
+    ----------
+    path
+        Path to directory for visium datafiles.
+    genome
+        Filter expression to genes within this genome.
+    count_file
+        Which file in the passed directory to use as the count file. Typically would be one of:
+        'filtered_feature_bc_matrix.h5' or 'raw_feature_bc_matrix.h5'.
+    library_id
+        Identifier for the visium library. Can be modified when concatenating multiple adata objects.
+    source_image_path
+        Path to the high-resolution tissue image. Path will be included in
+        `.uns["spatial"][library_id]["metadata"]["source_image_path"]`.
+
+    Returns
+    -------
+    Annotated data matrix ``adata``, where observations/cells are named by their
+    barcode and variables/genes by gene name. Stores the following information:
+    :attr:`~anndata.AnnData.X`
+        The data matrix is stored
+    :attr:`~anndata.AnnData.obs_names`
+        Cell names
+    :attr:`~anndata.AnnData.var_names`
+        Gene names
+    :attr:`~anndata.AnnData.var`\\ `['gene_ids']`
+        Gene IDs
+    :attr:`~anndata.AnnData.var`\\ `['feature_types']`
+        Feature types
+    :attr:`~anndata.AnnData.uns`\\ `['spatial']`
+        Dict of spaceranger output files with 'library_id' as key
+    :attr:`~anndata.AnnData.uns`\\ `['spatial'][library_id]['images']`
+        Dict of images (`'hires'` and `'lowres'`)
+    :attr:`~anndata.AnnData.uns`\\ `['spatial'][library_id]['scalefactors']`
+        Scale factors for the spots
+    :attr:`~anndata.AnnData.uns`\\ `['spatial'][library_id]['metadata']`
+        Files metadata: 'chemistry_description', 'software_version', 'source_image_path'
+    :attr:`~anndata.AnnData.obsm`\\ `['spatial']`
+        Spatial spot coordinates, usable as `basis` by :func:`~scanpy.pl.embedding`.
+    """
+    if isinstance(path, str):
+        path = Path(path)
+
+    adata = _read_count(path, count_file, genome)
+    library_id = Key.uns.library_id(adata, Key.uns.spatial, library_id)
+    if library_id is None:
+        raise ValueError(f"Invalid value for `library_id: {library_id}`. Cannot be None.")
+
+    image_files = {
+        "hires_image": path / f"{Key.uns.spatial}/tissue_hires_image.png",
+        "lowres_image": path / f"{Key.uns.spatial}/tissue_lowres_image.png",
+    }
+    image_dic = _read_images(image_files)
+
+    adata.uns[Key.uns.spatial][library_id][Key.uns.image_key] = image_dic
+
+    coords_scalefactors_files = {
+        "tissue_positions_file": path / f"{Key.uns.spatial}/tissue_positions_list.csv",
+        "scalefactors_json_file": path / f"{Key.uns.spatial}/scalefactors_json.json",
+    }
+
+    adata.uns[Key.uns.spatial][library_id]["scalefactors"] = json.loads(
+        coords_scalefactors_files["scalefactors_json_file"].read_bytes()
+    )
+
+    columns = [
+        "barcode",
+        "in_tissue",
+        "array_row",
+        "array_col",
+        "pxl_col_in_fullres",
+        "pxl_row_in_fullres",
+    ]
+
+    coords = _read_coords(
+        coords_scalefactors_files["tissue_positions_file"],
+        adata.shape[0],
+        columns,
+    )
+
+    coords.index = coords["barcode"].values  # type: ignore
+
+    adata.obs = adata.obs.join(coords, how="left")
+
+    adata.obsm[Key.uns.spatial] = adata.obs[["pxl_row_in_fullres", "pxl_col_in_fullres"]].to_numpy()
+    adata.obs.drop(
+        columns=["barcode", "pxl_row_in_fullres", "pxl_col_in_fullres"],
+        inplace=True,
+    )
+
+    # put image path in uns
+    if source_image_path is not None:
+        # get an absolute path
+        source_image_path = str(Path(source_image_path).resolve())
+        adata.uns[Key.uns.spatial][library_id]["metadata"]["source_image_path"] = str(source_image_path)
+    return adata
---
+++ squidpy/read/_utils.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+from h5py import File
+from types import MappingProxyType
+from typing import Any, Mapping, Optional, Sequence
+from imageio import imread
+from pathlib import Path
+
+from scanpy import read_10x_h5
+from anndata import AnnData, read_mtx, read_text
+
+import pandas as pd
+
+from squidpy._utils import NDArrayA
+from squidpy._constants._pkg_constants import Key
+
+
+def _read_count(
+    path: str | Path,
+    count_file: str,
+    genome: Optional[str] = None,
+    library_id: Optional[str] = None,
+    text_kwargs: Mapping[str, Any] = MappingProxyType({}),
+    mtx_kwargs: Mapping[str, Any] = MappingProxyType({}),
+) -> AnnData:
+    """Load count files."""
+    path = Path(path)
+    if count_file.endswith(".h5"):
+        adata = read_10x_h5(path / count_file, genome=genome)
+        with File(path / count_file, mode="r") as f:
+            attrs = dict(f.attrs)
+            if library_id is None:
+                try:
+                    library_id = str(attrs.pop("library_ids")[0], "utf-8")
+                except ValueError:
+                    raise ValueError(f"Invalid value for `library_id: {library_id}`. Cannot be None.")
+            adata.uns[Key.uns.spatial] = {library_id: {}}
+            metadata_dic = {
+                k: (str(attrs[k], "utf-8") if isinstance(attrs[k], bytes) else attrs[k])
+                for k in ("chemistry_description", "software_version")
+                if k in attrs
+            }
+            adata.uns[Key.uns.spatial][library_id]["metadata"] = metadata_dic
+        return adata
+    elif count_file.endswith((".csv", ".txt")):
+        return read_text(path / count_file, **text_kwargs)
+    elif count_file.endswith(".mtx"):
+        return read_mtx(path / count_file, **mtx_kwargs)
+
+
+def _read_images(
+    image_dic: Mapping[str, Path],
+) -> Mapping[str, NDArrayA]:
+    """Load images."""
+    dic = {}
+    for k, f in image_dic.items():
+        if not f.exists():
+            raise ValueError(f"Could not find: `{f}`")
+        dic[k] = imread(f)
+
+    return dic
+
+
+def _read_coords(
+    path: str | Path,
+    n_obs: int,
+    cols: Sequence[str] | None = None,
+    **kwargs: Any,
+) -> NDArrayA | pd.DataFrame:
+    """Load coordinates."""
+    coords = pd.read_csv(path, header=None, **kwargs)
+
+    if coords.shape[0] != n_obs:
+        raise ValueError(f"Invalid shape of `coordinates` file: `{coords.shape}`.")
+
+    if cols is None:
+        return coords.to_numpy()
+    else:
+        if len(cols) != coords.columns.shape[0]:
+            raise ValueError(f"Invalid length for columns: `{cols}`.")
+        coords.columns = cols
+        return coords
