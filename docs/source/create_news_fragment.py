df44c07a9315a1309dee112b9384669ec722c6dc
Michal Klein <michal.klein@protonmail.com>
2022-03-23 21:52:41 +0100

    Add author option when generating news

--- docs/source/create_news_fragment.py
+++ docs/source/create_news_fragment.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import List
+from typing import Any, List, Mapping, Optional
 from pathlib import Path
 import logging
 import argparse
@@ -44,7 +44,22 @@ def _parse_description(comment: str) -> str:
     raise ValueError("Unable to parse description from the issue comment.")
 
 
-def create_news_fragment(issue_number: str, use_title: bool = False, sep: str = "~") -> None:
+def _extract_pr_author(data: Mapping[str, Any]) -> Optional[str]:
+    try:
+        user = data["user"]["login"]
+    except KeyError as e:
+        logging.error(f"Unable to fetch user. Reason: `{e}`")
+        return None
+    try:
+        url = data["user"]["html_url"]
+    except KeyError as e:
+        url = f"https://github.com/{user}"
+        logging.warning(f"Unable to fetch user's url. Reason: `{e}`. Using `{url}`")
+
+    return f"`@{user} <{url}>`__"
+
+
+def create_news_fragment(issue_number: str, use_title: bool = False, add_author: bool = True, sep: str = "~") -> None:
     try:
         url = f"https://api.github.com/repos/theislab/squidpy/pulls/{issue_number}"
         resp = requests.get(url)
@@ -64,6 +79,7 @@ def create_news_fragment(issue_number: str, use_title: bool = False, sep: str = 
 
         title = str(data["title"]).strip()
         description = _parse_description(data["body"])
+        author = _extract_pr_author(data)
 
         fragment = f"{title}\n{len(title) * sep}\n{description}" if use_title else description
         fpath = _root / f"{issue_number}.{typ}.rst"
@@ -71,6 +87,9 @@ def create_news_fragment(issue_number: str, use_title: bool = False, sep: str = 
         logging.info(f"Saving news fragment into `{fpath}`")
         with open(fpath, "w") as fout:
             print(fragment, file=fout)
+            print(add_author, author)
+            if add_author and author is not None:
+                print(author, file=fout)
     except Exception as e:
         logging.error(f"Unable to generate news fragment. Reason: `{e}`")
 
@@ -89,9 +108,14 @@ if __name__ == "__main__":
         help="Whether to include the issue title in the fragment.",
     )
     parser.add_argument("-v", "--verbose", action="count", help="Be verbose.")
+    parser.add_argument("--add-author", action="store_true", help="Whether to append user who created the PR.")
 
     args = parser.parse_args()
     if args.verbose:
         logging.getLogger().setLevel(logging.INFO)
 
-    create_news_fragment(issue_number=args.issue_number, use_title=args.title)
+    create_news_fragment(
+        issue_number=args.issue_number,
+        use_title=args.title,
+        add_author=args.add_author,
+    )
