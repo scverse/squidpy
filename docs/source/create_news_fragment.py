3ba2ec22defc72c510e0782d6d9062b5e38c2bb8
Michal Klein <michal.klein@protonmail.com>
2022-03-23 22:19:44 +0100

    Use textwrap, regenerate fragment

--- docs/source/create_news_fragment.py
+++ docs/source/create_news_fragment.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import logging
 import argparse
 import requests
+import textwrap
 
 _root = Path(__file__).parent / "release" / "changelog"
 _valid_types = ("feature", "bugfix", "deprecation", "misc", "doc", "ignore-towncrier")
@@ -82,6 +83,7 @@ def create_news_fragment(issue_number: str, use_title: bool = False, add_author:
         author = _extract_pr_author(data)
 
         fragment = f"{title}\n{len(title) * sep}\n{description}" if use_title else description
+        fragment = "\n".join(textwrap.wrap(fragment, width=120, tabsize=4))
         fpath = _root / f"{issue_number}.{typ}.rst"
 
         logging.info(f"Saving news fragment into `{fpath}`")
