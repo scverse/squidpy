9220c61f188dd49d692797a58f7030a510bf6a39
giovp <giov.pll@gmail.com>
2022-04-10 15:36:05 +0200

    fix __init__.py

--- squidpy/pl/__init__.py
+++ squidpy/pl/__init__.py
@@ -8,9 +8,5 @@ from squidpy.pl._graph import (
 )
 from squidpy.pl._utils import extract
 from squidpy.pl._ligrec import ligrec
-from squidpy.pl._spatial import (  # spatial_point,
-    spatial_plot,
-    spatial_scatter,
-    spatial_segment,
-)
+from squidpy.pl._spatial import spatial_plot, spatial_scatter, spatial_segment
 from squidpy.pl._interactive import Interactive  # type: ignore[attr-defined]
