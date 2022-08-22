72e8c282f1356fd479f2771728ef6fd46e1bf7a0
giovp <giov.pll@gmail.com>
2022-08-22 12:40:23 +0200

    add tests

---
+++ squidpy/tl/__init__.py
@@ -0,0 +1,1 @@
+from squidpy.tl._exp_dist import exp_dist
---
+++ tests/tl/test_exp_dist.py
@@ -0,0 +1,21 @@
+from typing import List
+import pytest
+
+from anndata import AnnData
+
+
+@pytest.mark.parametrize("covariates", ["array_col", None])
+@pytest.mark.parametrize("groups", ["Cortex_1", ["Cortex_1", "Cortex_2", "Striatum"], None])
+@pytest.mark.parametrize("batch_key", ["library_id", None])
+def test_exp_dist(
+    adata_hne_concat: AnnData,
+    groups: str | List[str] | None,
+    batch_key: str | None,
+    covariates: str | List[str] | None,
+    cluster_key: str = "cluster",
+):
+    # TODO(LLehren): write tests for above parametrization
+    # check for dataframe shape, types of columns etc.
+    # general behaviour with different arguments passed
+    # catch correct warnings
+    assert True
