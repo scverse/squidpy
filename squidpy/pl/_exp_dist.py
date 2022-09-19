75c99ce56957fb3415403c14c8a388f7cbbabc2c
lehnerl <lehnerl@cip.ifi.lmu.de>
2022-09-20 00:07:00 +0200

    PR update

--- squidpy/pl/_exp_dist.py
+++ squidpy/pl/_exp_dist.py
@@ -1,68 +1,68 @@
-from typing import Union, Optional
-
-from anndata import AnnData
-import scanpy as sc
+from typing import Optional, Union
 
 import pandas as pd
-
+import seaborn as sns
+from anndata import AnnData
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 
-def plot_gexp_dist(
-    adata: AnnData,
-    design_matrix_key: str,
-    var: str,
-    n_bins: int = 20,
-    use_raw: Optional[bool] = False,
+def exp_dist(
+    adata: AnnData, design_matrix_key: str = None, var: str = None, n_bins: int = 20, use_raw: Optional[bool] = False
 ) -> Union[Figure, Axes, None]:
-    """Plot gene expression by distance to anchor point."""
+    """Plot gene expression by distance to anchor point. Several slides or anchor points possible"""
     if isinstance(var, str):
-        var = [var]
+        var = [var]  # type: ignore[assignment]
 
     dfs = {}
 
-    # get design matrix from anndata object
-    # TODO use this for getting the data from obsm if possible
-    # https://github.com/scverse/squidpy/blob/2cf664ffd9a1654b6d921307a76f5732305a371c/squidpy/pl/_graph.py#L32-L40
-    # although you'd have to modify with attribute and key
-    if use_raw:
-        df = adata.obsm[design_matrix_key + "_raw_dist"].copy()
+    df = _get_data(adata=adata, key=design_matrix_key, func_name="_exp_dist", attr="obsm")
 
+    if use_raw:
+        anchor_type = "anchor_raw"
+        df = df[
+            [
+                value
+                for key, value in adata.uns[design_matrix_key].items()
+                if "anchor_raw" in key or "annotation" in key or "batch" in key
+            ]
+        ]
     else:
-        df = adata.obsm[design_matrix_key].copy()
+        anchor_type = "anchor_scaled"
+        df = df[
+            [
+                value
+                for key, value in adata.uns[design_matrix_key].items()
+                if "anchor_scaled" in key or "annotation" in key or "batch" in key
+            ]
+        ]
 
     for v in var:
         # add var column to design matrix
         df[v] = sc.get.obs_df(adata, v).to_numpy()
 
-        metric = adata.uns[design_matrix_key]["metric"]
-        annotation = adata.uns[design_matrix_key]["annotation"]
-
         # set some plot settings depending on input
         if "batch_key" in adata.uns[design_matrix_key]:
             anchor = adata.uns[design_matrix_key]["batch_key"]
-            x_axis_desc = (
-                f'{metric} distance to {annotation} cluster {adata.uns[design_matrix_key]["anchor"]} ({n_bins} bins)'
-            )
+            x_axis_desc = f'{adata.uns[design_matrix_key]["metric"]} distance to {adata.uns[design_matrix_key]["annotation"]} cluster {adata.uns[design_matrix_key][anchor_type]} ({n_bins} bins)'
             # df = df.drop(adata.uns[design_matrix_key]["covariates"], axis=1)
-            df_melt = df.rename({str(adata.uns[design_matrix_key]["anchor"]): metric}, axis=1)
+            df_melt = df.rename(
+                {str(adata.uns[design_matrix_key][anchor_type]): adata.uns[design_matrix_key]["metric"]}, axis=1
+            )
         else:
             anchor = "anchor"
-            x_axis_desc = f"{metric} distance to anchor point ({n_bins} bins)"
+            x_axis_desc = f'{adata.uns[design_matrix_key]["metric"]} distance to anchor point ({n_bins} bins)'
             df_melt = df.melt(
-                id_vars=[v, annotation, "x", "y"],
+                id_vars=[v, adata.uns[design_matrix_key]["annotation"]],
                 var_name=anchor,
-                value_name=metric,
+                value_name=adata.uns[design_matrix_key]["metric"],
             )
 
         # sort by euclidean distance
-        df_melt.sort_values(metric, inplace=True)
+        df_melt.sort_values(adata.uns[design_matrix_key]["metric"], inplace=True)
 
         # create bins and get median from each binning interval
-        df_melt["bin"] = pd.cut(df_melt[metric], n_bins, include_lowest=True)
+        df_melt["bin"] = pd.cut(df_melt[adata.uns[design_matrix_key]["metric"]], n_bins, include_lowest=True)
 
         df_melt[x_axis_desc] = df_melt.apply(lambda row: row["bin"].mid, axis=1)
 
@@ -72,5 +72,21 @@ def plot_gexp_dist(
     for idx, v in enumerate(var):
         plt.subplot(1, len(var), idx + 1)
         plot = sns.lineplot(data=dfs[v], x=x_axis_desc, y=v, hue=anchor)
-        plot.set(xlim=(0, dfs[v][metric].max()))
+        plot.set(xlim=(0, dfs[v][adata.uns[design_matrix_key]["metric"]].max()))
+    # plt.savefig("exp_by_dist_pruned_anchor_tree.PDF")
     plt.show()
+
+
+# adapted from https://github.com/scverse/squidpy/blob/2cf664ffd9a1654b6d921307a76f5732305a371c/squidpy/pl/_graph.py#L32-L40
+def _get_data(adata: AnnData, key: str, func_name: str, attr: str = "obsm") -> Any:
+    try:
+        if attr == "obsm":
+            return adata.obsm[key]
+        elif attr == "uns":
+            return adata.uns[key]
+        else:
+            raise ValueError(f"attr must be either 'uns' or 'obsm', got {attr}")
+    except KeyError:
+        raise KeyError(
+            f"Unable to get the dat from 'adata.{attr}[{key}]'. " f"Please run `squidpy.tl.{func_name}' first."
+        )
--- squidpy/tl/_exp_dist.py
+++ squidpy/tl/_exp_dist.py
@@ -1,69 +1,79 @@
 from __future__ import annotations
-from typing import Union, Optional, Tuple
-from functools import reduce
-from itertools import product
 
 from anndata import AnnData
 from scanpy import logging as logg
-from sklearn.metrics import DistanceMetric
-from pandas.api.types import CategoricalDtype
-from sklearn.neighbors import KDTree
-from sklearn.preprocessing import MinMaxScaler
+from squidpy._constants._pkg_constants import Key
+from squidpy._docs import d
+from squidpy._utils import NDArrayA
+from squidpy.gr._utils import _save_data
+
+__all__ = ["exp_dist"]
+
+from functools import reduce
+from itertools import product
+from typing import Any, Dict, List, Optional
+
 import numpy as np
 import pandas as pd
-from squidpy._docs import d
-from squidpy.gr._utils import _save_data
-from squidpy._utils import NDArrayA
+from sklearn.metrics import DistanceMetric
+from sklearn.neighbors import KDTree
+from sklearn.preprocessing import MinMaxScaler
 
 
 @d.dedent
 def exp_dist(
     adata: AnnData,
-    annotation: str,
-    anchor: Union[str, list, np.ndarray],
-    metric: str = "euclidean",
+    groups: str | List[str] | NDArrayA,
+    cluster_key: str,
     design_matrix_key: str = "design_matrix",
     batch_key: str | None = None,
-    covariates: Union[str, list] | None = None,
+    covariates: str | List[str] | None = None,
+    spatial_key: str = Key.obsm.spatial,
+    metric: str = "euclidean",
     copy: bool = False,
 ) -> Optional[AnnData]:
     """
     Build a design matrix consisting of gene expression by distance to selected anchor point(s).
-
     Parameters
     ----------
     %(adata)s
     %(cluster_key)s
-    anchor
-        TODO
+    groups
+        anchor points to calculate distances from, can be a single str, a list of str or a numpy array of coordinates
+    cluster_key
+        annotation in .obs to take anchor point from
     metric
-        TODO
+        distance metric, defaults to "euclidean"
     design_matrix
-        TODO
+        name of the design matrix saved to .obsm, defaults to "design_matrix"
     batch_key
-        TODO
+        optional: specifiy batches which contain identical anchor points
     covariates
-        TODO
-
+        additional covariates from .obs which can be used for modelling
     Returns
     -------
-    TODO
-    If ``copy = True``, returns the design_matrix and the distance thresholds intervals TODO....
-
-    Otherwise, TODO
+    If ``copy = True``, returns the design_matrix and the distance thresholds intervals
+    Otherwise, stores design_matrix in .obsm
     """
+    start = logg.info(f"Creating {design_matrix_key}")
     # list of columns which will be categorical later on
-    categorical_columns = [annotation]
+    categorical_columns = [cluster_key]
 
     # save initial metadata to adata.uns
-    adata.uns[design_matrix_key] = _add_metadata(adata, annotation, anchor, metric, batch_key, covariates)
+    adata.uns[design_matrix_key] = _add_metadata(
+        adata, cluster_key, groups, metric=metric, batch_key=batch_key, covariates=covariates
+    )
 
-    anchor_count = 0
-    if isinstance(anchor, str) or isinstance(anchor, np.ndarray):
-        anchor = [anchor]
-        anchor_count = 1
+    # anchor_count = 0
+    # if isinstance(groups, np.ndarray):
+    # anchor: List[str | None] = [None] # if None, then assumed ArrayLike
+    # else:
+    anchor = (
+        [groups] if isinstance(groups, str) or isinstance(groups, np.ndarray) else groups
+    )  # type: ignore [assignment]
 
     # prepare batch key for iteration (Nonetype alone in product will result in neutral element)
+    # batch = [batch_key] if batch_key is None else adata.obs[batch_key].cat.categorical
     if batch_key is None:
         batch = [None]
     else:
@@ -76,21 +86,26 @@ def exp_dist(
     for batch_var, anchor_var in product(batch, anchor):
         # initialize dataframe and anndata depending on whether batches are used or not
         if batch_var is not None:
-            df = _init_design_matrix(adata[adata.obs[batch_key] == batch_var], True, annotation, anchor_var, batch_key)
+            df = _init_design_matrix(adata, cluster_key, spatial_key, batch_key=batch_key, batch_var=batch_var)
             anchor_coord, batch_coord = _get_coordinates(
-                adata[adata.obs[batch_key] == batch_var], anchor_var, annotation
+                adata[adata.obs[batch_key] == batch_var], anchor_var, cluster_key
             )
 
         else:
-            df = _init_design_matrix(adata, False, annotation, anchor_var)
-            anchor_coord, batch_coord = _get_coordinates(adata, anchor_var, annotation)
+            df = _init_design_matrix(adata, cluster_key, spatial_key)
+            anchor_coord, batch_coord = _get_coordinates(adata, anchor_var, cluster_key)
+
+        anchor_coord = _prune_anchor_tree(anchor_coord, 0.05, 4, metric)
 
         tree = KDTree(anchor_coord, metric=DistanceMetric.get_metric(metric))
         mindist, _ = tree.query(batch_coord)
 
+        # test later
+        # ARRAY = ARRAY[ARRAY >= some_condition]
         if isinstance(anchor_var, np.ndarray):
             anchor_var = "custom_anchor"
-        df.insert(loc=2, column=str(anchor_var), value=mindist)
+            anchor = ["custom_anchor"]
+        df.insert(loc=1, column=str(anchor_var), value=mindist)
         if batch_var is not None:
             df["obs"] = adata[adata.obs[batch_key] == batch_var].obs_names
         else:
@@ -100,9 +115,10 @@ def exp_dist(
     # merge individual data frames
     # use merge when several anchor points were used and concat when one anchor but several slides were used
     # if a single anchor point with a single batch is used take design matrix directly
-    if batch_key is None and anchor_count == 0:
+
+    if batch_key is None and len(anchor) > 1:
         df = reduce(
-            lambda df1, df2: pd.merge(df1, df2, on=[annotation, "x", "y", "obs"]),
+            lambda df1, df2: pd.merge(df1, df2, on=[cluster_key, "obs"]),
             list(batch_design_matrices.values()),
         )
         df.set_index("obs", inplace=True)
@@ -110,74 +126,64 @@ def exp_dist(
     elif batch_key is not None:
         df = pd.concat(list(batch_design_matrices.values()))
         df = df.reindex(adata.obs_names)
+        df = df.drop("obs", axis=1)
     else:
-        df = batch_design_matrices[str((batch_var, anchor_var))]
+        df = batch_design_matrices[str((batch_var, anchor_var))].drop("obs", axis=1)
 
     # normalize euclidean distances column(s)
-    df = _normalize_distances(adata, df, anchor, design_matrix_key)
+    df = _normalize_distances(df, anchor)
 
     # add additional covariates to design matrix
     if covariates is not None:
         if isinstance(covariates, str):
             covariates = [covariates]
-        for covariate in covariates:
-            df[covariate] = adata.obs[covariate]
-            adata.obsm[design_matrix_key + "_raw_dist"][covariate] = adata.obs[covariate]
-            categorical_columns.append(covariate)
+        df[covariates] = adata.obs[covariates].copy()
 
     # organize data frames after merging, depending if batches were used or not
-    adata.obsm[design_matrix_key + "_raw_dist"] = adata.obsm[design_matrix_key + "_raw_dist"][
-        [value for key, value in adata.uns[design_matrix_key].items() if key not in ["metric"]]
-    ]
-    df = df[[value for key, value in adata.uns[design_matrix_key].items() if key not in ["metric"]]]
+    # adata.obsm[design_matrix_key + "_raw_dist"] = adata.obsm[design_matrix_key + "_raw_dist"][[value for key, value in adata.uns[design_matrix_key].items() if key not in ["metric"]]]
+    # df = df[[value for key, value in adata.uns[design_matrix_key].items() if key not in ["metric"]]]
 
     # make sure that columns without numerical values are of type categorical
-    for cat_name in categorical_columns:
-        if isinstance(cat_name, CategoricalDtype):
-            continue
-        else:
-            df[cat_name] = pd.Categorical(df[cat_name])
+    # for cat_name in categorical_columns:
+    #    if isinstance(cat_name, CategoricalDtype):
+    #        continue
+    #    else:
+    #        df[cat_name] = pd.Categorical(df[cat_name])
 
     if copy:
         return df
-
-    # save design matrix dataframe to adata.obsm
-    # TODO remove and use _save_data instead, see below
-    # see https://github.com/scverse/squidpy/blob/2cf664ffd9a1654b6d921307a76f5732305a371c/squidpy/gr/_ppatterns.py#L398-L404
-    # adata.obsm[design_matrix_key] = df
-
-    # _save_data(
-    #     adata, attr="uns", key=Key.uns.co_occurrence(cluster_key), data={"occ": out, "interval": interval}, time=start
-    # )
+    else:
+        # adapted from https://github.com/scverse/squidpy/blob/2cf664ffd9a1654b6d921307a76f5732305a371c/squidpy/gr/_ppatterns.py#L398-L404
+        return _save_data(adata, attr="obsm", key=design_matrix_key, data=df, time=start)
 
 
 def _add_metadata(
     adata: AnnData,
-    annotation: str,  # categorical
-    anchor: Union[str, list, np.ndarray],
-    batch_key: Optional[str],  # categorical
-    covariates: Optional[Union[str, list]],
+    cluster_key: str,
+    groups: str | List[str] | NDArrayA,
+    batch_key: str | None = None,
+    covariates: str | List[str] | None = None,
     metric: str = "euclidean",
-):
+) -> Dict[str, Any]:
     """Add metadata to adata.uns."""
     metadata = {}
-    if isinstance(anchor, np.ndarray):
-        metadata["anchor"] = "custom_anchor"
-    elif isinstance(anchor, list):
-        for i, a in enumerate(anchor):
-            metadata["anchor_" + str(i)] = a
+    if isinstance(groups, np.ndarray):
+        metadata["anchor_scaled"] = "custom_anchor"
+        metadata["anchor_raw"] = "custom_anchor_raw"
+    elif isinstance(groups, list):
+        for i, anchor in enumerate(groups):
+            metadata["anchor_scaled" + str(i)] = anchor
+            metadata["anchor_raw" + str(i)] = anchor + "_raw"
     else:
-        metadata["anchor"] = anchor
+        metadata["anchor_scaled"] = groups
+        metadata["anchor_raw"] = groups + "_raw"
 
-    metadata["annotation"] = annotation
+    metadata["annotation"] = cluster_key
 
     if batch_key is not None:
         metadata["batch_key"] = batch_key
 
-    metadata["x"] = "x"
-    metadata["y"] = "y"
-
-    metadata["metric"] = "euclidean"
+    metadata["metric"] = metric
 
     if covariates is not None:
         if isinstance(covariates, str):
@@ -189,43 +195,80 @@ def _add_metadata(
 
 
 def _init_design_matrix(
-    adata: AnnData, batch: bool, annotation: str, anchor: str, batch_key: Optional[str]
+    adata: AnnData, cluster_key: str, spatial_key: str, batch_key: Optional[str] = None, batch_var: Optional[str] = None
 ) -> pd.DataFrame:
     """Initialize design matrix."""
-    df = adata.obs[[annotation]]
-    if batch:
-        df[batch_key] = adata.obs[batch_key]
-    df["x"] = adata.obsm["spatial"][:, 0]
-    df["y"] = adata.obsm["spatial"][:, 1]
-
-    return df
-
-
-def _normalize_distances(adata: AnnData, df: pd.DataFrame, anchor: list, design_matrix_key: str) -> pd.DataFrame:
-    """Normalize distances to anchor."""
-    # .values used for fit and transform to avoid:
-    # "FutureWarning: The feature names should match those that were passed during fit.
-    # Starting version 1.2, an error will be raised."
-    scaler = MinMaxScaler()
-    raw_dist = df.copy()
-    if "custom_anchor" in df.columns:
-        anchor = ["custom_anchor"]
-    if len(anchor) > 1:
-        max_dist_anchor = df[anchor].columns[np.where(df[anchor].values == np.max(df[anchor].values))[1]][0]
-        scaler.fit(df[[max_dist_anchor]].values)
-        df[max_dist_anchor] = scaler.transform(df[[max_dist_anchor]].values)
-        anchor.remove(max_dist_anchor)
-        for a in anchor:
-            df[a] = scaler.transform(df[[a]].values)
+    if batch_key is not None and batch_var is not None:
+        df = adata[adata.obs[batch_key] == batch_var].obs[[cluster_key]].copy()
+        df[batch_key] = adata[adata.obs[batch_key] == batch_var].obs[batch_key].copy()
     else:
-        df[anchor] = scaler.fit_transform(df[anchor].values)
-    adata.obsm[design_matrix_key + "_raw_dist"] = raw_dist
+        df = adata.obs[[cluster_key]].copy()
+
     return df
 
 
-def _get_coordinates(adata: AnnData, anchor: str, annotation: str) -> Tuple(NDArrayA, NDArrayA):
+def _get_coordinates(adata: AnnData, anchor: str = None, annotation: str = None) -> Tuple(NDArrayA, NDArrayA):
     """Get anchor coordinates and coordinates of all observations."""
     if isinstance(anchor, np.ndarray):
         return (anchor, adata.obsm["spatial"])
     else:
         return (np.array(adata[adata.obs[annotation] == anchor].obsm["spatial"]), adata.obsm["spatial"])
+
+
+def _prune_anchor_tree(anchor_coord: np.ndarray, pct_node_to_filter: float, nth_nbor: int, metric: str) -> np.ndarray:
+    """
+    Prune the anchor coordinate tree to keep the anchor point dense.
+    ----------
+    pct_node_to_filter
+        Percentage of anchor point spots to be removed.
+    nth_nbor
+        Filter based on the distance to nth neighbor. If a single spot belonging to an anchor point
+        is distant to the main region of the anchor point, then a value of 1 will filter the spot
+        if there is a cluster of spots distant to the main region of the anchor point, the value needs to be greater
+        than the amount of spots in the cluster, to filter the entire cluster.
+    Returns
+    -------
+    A np.ndarray of anchor coordinates.
+    """
+
+    # query the anchor point spots against each other to get the distances between all spots within the anchor point
+    anchor_tree = KDTree(anchor_coord, metric=DistanceMetric.get_metric(metric))
+    anchor_dist, anchor_id = anchor_tree.query(anchor_coord, k=nth_nbor)
+
+    # m amount of nodes to filter in the tree
+    top_n_dist = round(len(anchor_dist) * pct_node_to_filter)
+
+    # for all nodes in the tree, get the distances to the nth neighbor
+    nth_nbor_dist = anchor_dist[..., (nth_nbor - 1)].ravel()
+
+    # get the nodes which have the top m highest distances to the nth neighbor
+    top_indices = np.argpartition(nth_nbor_dist, -top_n_dist)[-top_n_dist:]
+
+    # from the tree remove the nodes with the m highest distances to the nth neighbor
+    to_keep = np.ones(len(anchor_dist), dtype=bool)
+    to_keep[top_indices] = False
+    anchor_coord = anchor_coord[to_keep]
+
+    return anchor_coord
+
+
+def _normalize_distances(
+    df: pd.DataFrame,
+    anchor: list = None,
+) -> pd.DataFrame:
+    """Normalize distances to anchor."""
+    scaler = MinMaxScaler()
+    if len(anchor) > 1:
+        max_dist_anchor = df[anchor].columns[np.where(df[anchor].values == np.max(df[anchor].values))[1]][0]
+        scaler.fit(df[[max_dist_anchor]].values)
+        df[f"{max_dist_anchor}_raw"] = df[max_dist_anchor]
+        df[max_dist_anchor] = scaler.transform(df[[max_dist_anchor]].values)
+        anchor.remove(max_dist_anchor)
+        for a in anchor:
+            df[f"{a}_raw"] = df[a]
+            df[a] = scaler.transform(df[[a]].values)
+
+    else:
+        df[f"{anchor[0]}_raw"] = df[anchor]
+        df[anchor] = scaler.fit_transform(df[anchor].values)
+    return df
