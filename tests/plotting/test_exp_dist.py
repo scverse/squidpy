0dc011c7ec5c3d1aa930bb3efd0426dc2783cd48
LLehner <laurenslehner@googlemail.com>
2023-03-21 15:25:06 +0100

    added test back

---
+++ tests/plotting/test_exp_dist.py
@@ -0,0 +1,25 @@
+import scanpy as sc
+from anndata import AnnData
+
+from squidpy import pl
+from squidpy.tl import exp_dist
+from tests.conftest import PlotTester, PlotTesterMeta
+
+# WARNING:
+# 1. all classes must both subclass PlotTester and use metaclass=PlotTesterMeta
+# 2. tests which produce a plot must be prefixed with `test_plot_`
+# 3. if the tolerance needs to be change, don't prefix the function with `test_plot_`, but with something else
+#    the comp. function can be accessed as `self.compare(<your_filename>, tolerance=<your_tolerance>)`
+#    ".png" is appended to <your_filename>, no need to set it
+
+
+class TestSpatialStatic(PlotTester, metaclass=PlotTesterMeta):
+    def test_tol_plot_exp_dist(self, adata_mibitof: AnnData):
+        exp_dist(
+            adata_mibitof,
+            cluster_key="Cluster",
+            groups="Epithelial",
+            library_key="point",
+        )
+        pl.exp_dist(adata=adata_mibitof, design_matrix_key="design_matrix", var="HK1", anchor_key="Epithelial", color="HK1", dpi=200)
+        self.compare("Exp_dist_single_anchor_one_gene", tolerance=70)
