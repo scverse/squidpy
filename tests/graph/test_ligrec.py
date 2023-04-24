de6351efb407ae3fbc3b339888a8225124fdb6f6
Laurens Lehner <laurenslehner@googlemail.com>
2023-04-24 16:14:51 +0200

    Update reqs and tests

--- tests/graph/test_ligrec.py
+++ tests/graph/test_ligrec.py
@@ -33,7 +33,9 @@ class TestInvalidBehavior:
 
     def test_raw_has_different_n_obs(self, adata: AnnData):
         adata.raw = blobs(n_observations=adata.n_obs + 1)
-        with pytest.raises(ValueError, match=rf"Expected `{adata.n_obs}` cells in `.raw`"):
+        # raise below happend with anndata < 0.9
+        # with pytest.raises(ValueError, match=rf"Expected `{adata.n_obs}` cells in `.raw`"):
+        with pytest.raises(ValueError, match=rf"Index length mismatch: {adata.n_obs} vs. {adata.n_obs + 1}"):
             ligrec(adata, _CK)
 
     def test_invalid_cluster_key(self, adata: AnnData, interactions: Interactions_t):
