ffe08670a3a13095baea273c7a534aeb16a8b9ae
giovp <giov.pll@gmail.com>
2023-06-03 20:26:33 +0200

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd
 import pytest
 import scanpy as sc
+from __future_ import annotations
 from anndata import AnnData, OldFormatWarning
 from matplotlib.testing.compare import compare_images
 from scipy.sparse import csr_matrix
