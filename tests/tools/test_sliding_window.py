59484b99a63f31c78a969b048351a15d1864900f
Tim Treis <tim.treis@stud.uni-heidelberg.de>
2024-10-30 22:53:13 -0400

    fixed test

--- tests/tools/test_sliding_window.py
+++ tests/tools/test_sliding_window.py
@@ -10,30 +10,24 @@ from squidpy.tl import _calculate_window_corners, sliding_window
 
 
 class TestSlidingWindow:
-    @pytest.mark.parametrize("library_key", ["library_id"])
-    @pytest.mark.parametrize("window_size", [200])
     @pytest.mark.parametrize("overlap", [0, 20])
-    @pytest.mark.parametrize("sliding_window_key", ["sliding_window", "sliding_window_v2"])
     def test_sliding_window_several_slices(
         self,
         adata_mibitof: AnnData,
-        library_key: str,
-        window_size: int,
         overlap: int,
-        sliding_window_key: str,
+        sliding_window_key: str = "sliding_window_key",
+        window_size: int = 200,
+        library_key: str = "library_id",
     ):
-        if window_size <= 0 or overlap < 0:
-            pytest.xfail("unsupported configuration")
-        else:
-            df = sliding_window(
-                adata_mibitof,
-                library_key=library_key,
-                window_size=window_size,
-                overlap=overlap,
-                coord_columns=("globalX", "globalY"),
-                sliding_window_key=sliding_window_key,
-                copy=True,
-            )
+        df = sliding_window(
+            adata_mibitof,
+            library_key=library_key,
+            window_size=window_size,
+            overlap=overlap,
+            coord_columns=("globalX", "globalY"),
+            sliding_window_key=sliding_window_key,
+            copy=True,
+        )
 
         assert len(df) == adata_mibitof.n_obs  # correct amount of rows
 
@@ -42,50 +36,39 @@ class TestSlidingWindow:
             assert len(sliding_window_columns) == 1  # only one sliding window
             assert df[sliding_window_key].isnull().sum() == 0  # no unassigned cells
         else:
-            grid_len = np.ceil(window_size / overlap)
-            num_grid_systems = grid_len**2
-            for i in range(num_grid_systems):
+            for i in range(36):  # we expect 36 windows
                 assert (
-                    f"{sliding_window_key}_{i+1}" in df.columns
+                    f"{sliding_window_key}_window_{i}" in df.columns
                 )  # correct number of columns; multiple sliding windows
 
-            for col in df.columns:
-                lib_windows = [
-                    set(df[adata_mibitof.obs[library_key] == lib][col])
-                    for lib in adata_mibitof.obs[library_key].unique()
-                ]
-                assert (
-                    len(set.intersection(*lib_windows)) == 0
-                )  # no intersection of sliding windows across library_keys
-
-    @pytest.mark.parametrize("window_size", [10])
     @pytest.mark.parametrize("overlap", [0, 2])
     def test_sliding_window_square_grid(
         self,
         adata_squaregrid: AnnData,
-        window_size: int,
         overlap: int,
+        sliding_window_key: str = "sliding_window_key",
+        window_size: int = 5,
     ):
         df = sliding_window(
             adata_squaregrid,
             window_size=window_size,
             overlap=overlap,
             coord_columns=("globalX", "globalY"),
-            sliding_window_key="sliding_window",
+            sliding_window_key=sliding_window_key,
             copy=True,
         )
 
         assert len(df) == adata_squaregrid.n_obs  # correct amount of rows
 
         if overlap == 0:
-            sliding_window_columns = [col for col in df.columns if "sliding_window" in col]
+            sliding_window_columns = [col for col in df.columns if sliding_window_key in col]
             assert len(sliding_window_columns) == 1  # only one sliding window
-            assert df["sliding_window"].isnull().sum() == 0  # no unassigned cells
+            assert df[sliding_window_key].isnull().sum() == 0  # no unassigned cells
         else:
-            grid_len = window_size // overlap
-            num_grid_systems = grid_len**2
-            for i in range(num_grid_systems):
-                assert f"sliding_window_{i+1}" in df.columns  # correct number of columns; multiple sliding windows
+            for i in range(9):  # we expect 9 windows
+                assert (
+                    f"{sliding_window_key}_window_{i}" in df.columns
+                )  # correct number of columns; multiple sliding windows
 
     def test_sliding_window_invalid_window_size(
         self,
