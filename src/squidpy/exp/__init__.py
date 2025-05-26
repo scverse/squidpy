953c8c193c110bf116f9512d07585859b583de99
Sonja Stockhaus <stockhaus@cip.ifi.lmu.de>
2025-05-26 12:21:56 +0200

    first version of _spatial_scatter for sdata

---
+++ src/squidpy/exp/__init__.py
@@ -0,0 +1,13 @@
+"""Experimental module for Squidpy.
+
+This module contains experimental features that are still under development.
+These features may change or be removed in future releases.
+"""
+
+from __future__ import annotations
+
+from squidpy.exp import pl
+
+# from squidpy.exp.pl._spatial import spatial_scatter
+
+# __all__ = ["spatial_scatter"]
---
+++ src/squidpy/exp/pl/__init__.py
@@ -0,0 +1,5 @@
+"""The experimental plotting module."""
+
+from __future__ import annotations
+
+from squidpy.exp.pl._spatial import spatial_scatter
