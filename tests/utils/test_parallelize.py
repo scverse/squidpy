03384f4643917c46ea4a31e04b458998ec3ad6d3
selman.ozleyen <syozleyen@gmail.com>
2025-06-05 15:02:27 +0200

    finalize the tests

--- tests/utils/test_parallelize.py
+++ tests/utils/test_parallelize.py
@@ -6,34 +6,39 @@ import numpy as np
 import pytest  # type: ignore[import]
 import numba
 import dask.array as da
-from typing import Callable
+from collections.abc import Callable
 from functools import partial
+import psutil
 
 from squidpy._utils import parallelize, Signal
 
 
-
 # Functions to be parallelized
+
 
 @numba.njit(parallel=True)
 def numba_parallel_func(x, y) -> np.ndarray:
     return x * 2 + y
 
+
 @numba.njit(parallel=False)
 def numba_serial_func(x, y) -> np.ndarray:
     return x * 2 + y
 
+
 def dask_func(x, y) -> np.ndarray:
     return (da.from_array(x) * 2 + y).compute()
+
 
 def vanilla_func(x, y) -> np.ndarray:
     return x * 2 + y
 
+
 # Mock runner function
+
 
 def mock_runner(x, y, queue, func):
     for i in range(len(x)):
-        print(len(x[i]), len(y))
         x[i] = func(x[i], y)
         if queue is not None:
             queue.put(Signal.UPDATE)
@@ -52,15 +57,40 @@ def func(request) -> Callable:
     }[request.param]
 
 
+@pytest.mark.isolated
 @pytest.mark.parametrize("n_jobs", [1, 2, 8])
 def test_parallelize_loky(func, n_jobs):
+    seed = 42
+    rng = np.random.RandomState(seed)
     n = 8
-    arr1 = [np.arange(n) for _ in range(n)]
+    arr1 = [rng.randint(0, 100, n) for _ in range(n)]
     arr2 = np.arange(n)
     runner = partial(mock_runner, func=func)
     expected = [func(arr1[i], arr2) for i in range(len(arr1))]
-    p_func = parallelize(runner, arr1, n_jobs=n_jobs, backend="loky", use_ixs=False)
+    old_num_threads = 3
+    numba.set_num_threads(old_num_threads)
+    # Get initial state
+    initial_process = psutil.Process()
+    initial_children = {p.pid for p in initial_process.children(recursive=True)}
+    init_numba_threads = numba.get_num_threads()
+
+    p_func = parallelize(runner, arr1, n_jobs=n_jobs, backend="loky", use_ixs=False, n_split=1)
     result = p_func(arr2)[0]
+
+    final_children = {p.pid for p in initial_process.children(recursive=True)}
+    final_numba_threads = numba.get_num_threads()
+
+    assert init_numba_threads == old_num_threads, "Numba threads should not change"
+    assert final_numba_threads == 1, "Numba threads should be 1"
     assert len(result) == len(expected), f"Expected: {expected} but got {result}. Length mismatch"
     for i in range(len(arr1)):
         assert np.all(result[i] == expected[i]), f"Expected {expected[i]} but got {result[i]}"
+
+    tracker_pname = "joblib.externals.loky.backend.resource_tracker"
+    processes = final_children - initial_children
+    processes = {psutil.Process(pid) for pid in processes}
+    processes = {p for p in processes if not any(tracker_pname in cl for cl in p.cmdline())}
+    if n_jobs > 1:  # expect exactly n_jobs
+        assert len(processes) == n_jobs, f"Unexpected new processes created: {processes}"
+    else:  # some functions use the main process others use a new process
+        assert len(processes) <= 1, f"Unexpected new processes created: {processes}"
