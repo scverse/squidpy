719b69653fbe563f7fba6f78b338b9b5839989f0
selmanozleyen <syozleyen@gmail.com>
2025-07-09 14:00:13 +0200

    correct the docstring

--- src/squidpy/pp/_simple.py
+++ src/squidpy/pp/_simple.py
@@ -29,8 +29,8 @@ def filter_cells(
 
 
     - labels: filtered based on the values of the images which are assumed to be the instance_id.
-    - shapes: filtered based on the index which is assumed to be the instance_id.
-    - points: filtered based on the instance_id column.
+    - points: filtered based on the index which is assumed to be the instance_id.
+    - shapes: filtered based on the instance_id column.
 
 
     See :func:`scanpy.pp.filter_cells` for more details regarding the filtering
@@ -125,74 +125,3 @@ def _filter_cells_spatialdata(
     if inplace:
         return None
     return data_out
-
-
-def _filter_shapesmodel_by_instance_ids(element: ShapesModel, ids_to_remove: list[str]) -> ShapesModel:
-    return element[~element.index.isin(ids_to_remove)]
-
-
-def _filter_labels2dmodel_by_instance_ids(element: Labels2DModel, ids_to_remove: list[int]) -> Labels2DModel:
-    def set_ids_in_label_to_zero(image: xr.DataArray, ids_to_remove: list[int]) -> xr.DataArray:
-        # Use apply_ufunc for efficient processing
-        def _mask_block(block: xr.DataArray) -> xr.DataArray:
-            # Create a copy to avoid modifying read-only array
-            result = block.copy()
-            result[np.isin(result, ids_to_remove)] = 0
-            return result
-
-        processed = xr.apply_ufunc(
-            _mask_block,
-            image,
-            input_core_dims=[["y", "x"]],
-            output_core_dims=[["y", "x"]],
-            vectorize=True,
-            dask="parallelized",
-            output_dtypes=[image.dtype],
-            dataset_fill_value=0,
-            dask_gufunc_kwargs={"allow_rechunk": True},
-        )
-
-        # Force computation to ensure the changes are materialized
-        computed_result = processed.compute()
-
-        # Create a new DataArray to ensure persistence
-        result = xr.DataArray(
-            data=computed_result.data,
-            coords=image.coords,
-            dims=image.dims,
-            attrs=image.attrs.copy(),  # Preserve all attributes
-        )
-
-        return result
-
-    if isinstance(element, xr.DataArray):
-        return Labels2DModel.parse(set_ids_in_label_to_zero(element, ids_to_remove))
-
-    if isinstance(element, DataTree):
-        # we extract the info to just reconstruct the DataTree after filtering the max scale
-        max_scale = list(element.keys())[0]
-        scale_factors = _get_scale_factors(element)
-        scale_factors = [int(sf[0]) for sf in scale_factors]
-
-        return Labels2DModel.parse(
-            data=set_ids_in_label_to_zero(element[max_scale].image, ids_to_remove),
-            scale_factors=scale_factors,
-        )
-
-
-def _get_scale_factors(labels_element: Labels2DModel) -> list[tuple[float, float]]:
-    scales = list(labels_element.keys())
-
-    # Calculate relative scale factors between consecutive scales
-    scale_factors = []
-    for i in range(len(scales) - 1):
-        y_size_current = labels_element[scales[i]].image.shape[0]
-        x_size_current = labels_element[scales[i]].image.shape[1]
-        y_size_next = labels_element[scales[i + 1]].image.shape[0]
-        x_size_next = labels_element[scales[i + 1]].image.shape[1]
-        y_factor = y_size_current / y_size_next
-        x_factor = x_size_current / x_size_next
-
-        scale_factors.append((y_factor, x_factor))
-
-    return scale_factors
