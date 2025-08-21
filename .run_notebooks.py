41bff5eedc323505d84714047f81b9883a36cfbf
selman.ozleyen <syozleyen@gmail.com>
2025-08-21 13:55:38 +0200

    use uv and hatch for notebooks only

--- .run_notebooks.py
+++ .run_notebooks.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from __future__ import annotations
 
 import argparse
 import glob
@@ -13,7 +14,7 @@ Examples:
 """
 
 
-def main():
+def main() -> None:
     # Set up argument parser
     parser = argparse.ArgumentParser(
         description="Run Jupyter notebooks in specified directories using jupytext",
