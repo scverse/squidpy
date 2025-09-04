7ae4c2d7b6571f1030e8ad92d6d583344e023b93
selman.ozleyen <syozleyen@gmail.com>
2025-09-04 13:13:26 +0200

    give it as condition expression

--- tests/datasets/conftest.py
+++ tests/datasets/conftest.py
@@ -8,4 +8,4 @@ import pytest
 @pytest.fixture(autouse=True)
 def _xfail_internet_if_macos(request: pytest.FixtureRequest) -> None:
     if request.node.get_closest_marker("internet") and sys.platform == "darwin":
-        request.applymarker(pytest.mark.xfail("Downloads fail on macOS", strict=False))
+        request.applymarker(pytest.mark.xfail(reason="Downloads fail on macOS", strict=False))
