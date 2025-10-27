900d7b31515d19c0323c9e4a8a3896c2f1ab796e
Tim Treis <tim.treis@helmholtz-muenchen.de>
2025-10-27 16:05:10 +0100

    Method to detect specimens in H&E images (#1044)
    * mvp for function; without testgs
    
    * added option to retain holes
    
    * refactor + 1 test
    
    * added missing import
    
    * renamed test so that a plot would be generated
    
    * added img from runner; cross-os-data-cache
    
    * improved docstring
    
    * added data download script to correct location
    
    * updated hatch commands
    
    * modified coverage combine
    
    * removed superflous combine step
    
    * first download data, then run tests
    
    * attempt to simplify
    
    * aligned testing
    
    * updated toml
    
    * aligned __init__ files
    
    * no uv cache for data download
    
    * removed download step that'd never get hit
    
    * simplify
    
    * parallel
    
    * speed up tests
    
    ---------
    
    Co-authored-by: Phil Schaf <flying-sheep@web.de>

---
+++ src/squidpy/experimental/im/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from ._detect_tissue import (
+    BackgroundDetectionParams,
+    FelzenszwalbParams,
+    detect_tissue,
+)
+
+__all__ = ["detect_tissue", "BackgroundDetectionParams", "FelzenszwalbParams"]
