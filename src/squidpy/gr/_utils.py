3ee6ef291fb89a31c27c528f62e777a6b0ebca99
selmanozleyen <syozleyen@gmail.com>
2025-11-07 10:33:59 +0100

    init

--- src/squidpy/gr/_utils.py
+++ src/squidpy/gr/_utils.py
@@ -2,11 +2,11 @@
 
 from __future__ import annotations
 
+import importlib.metadata
 from collections.abc import Hashable, Iterable, Sequence
 from contextlib import contextmanager
 from typing import TYPE_CHECKING, Any
 
-import anndata
 import numpy as np
 import pandas as pd
 from anndata import AnnData
@@ -20,7 +20,7 @@ from scipy.sparse import csc_matrix, csr_matrix, issparse, spmatrix
 from squidpy._docs import d
 from squidpy._utils import NDArrayA, _unique_order_preserving
 
-CAN_USE_SPARSE_ARRAY = Version(anndata.__version__) >= Version("0.11.0rc1")
+CAN_USE_SPARSE_ARRAY = Version(importlib.metadata.version("anndata")) >= Version("0.11.0rc1")
 if CAN_USE_SPARSE_ARRAY:
     from anndata._core.views import ArrayView
     from anndata._core.views import SparseCSCMatrixView as SparseCSCView
