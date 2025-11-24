ce2da2b3a1564660f20353652077e7e2c36174f4
Tim Treis <tim.treis@stud.uni-heidelberg.de>
2025-11-24 13:19:52 +0100

    updated tests for correct plot

--- tests/experimental/test_make_tiles.py
+++ tests/experimental/test_make_tiles.py
@@ -15,34 +15,28 @@ class TestMakeTiles(PlotTester, metaclass=PlotTesterMeta):
 
         sq.experimental.im.make_tiles(sdata, image_key="hne")
 
-        sdata.pl.render_shapes("hne_tiles", color="tile_classification", fill_alpha=0.5).pl.show()
-
 
 class TestMakeTilesFromSpots(PlotTester, metaclass=PlotTesterMeta):
     def test_plot_make_tiles_from_spots(self):
         """Test make tiles from spots on Visium H&E dataset."""
         sdata = sq.datasets.visium_hne_sdata()
 
+        # background cannot be classified, no img or mask, grey tiles in plot
         sq.experimental.im.make_tiles_from_spots(
             sdata,
             spots_key="spots",
         )
-
-        # background cannot be classified, no img or mask, grey tiles in plot
-        sdata.pl.render_shapes("spots_tiles", color="tile_classification", fill_alpha=0.5).pl.show()
 
     def test_plot_make_tiles_from_spots_with_image_key(self):
         """Test make tiles from spots with image key on Visium H&E dataset."""
         sdata = sq.datasets.visium_hne_sdata()
 
+        # background is automatically classified
         sq.experimental.im.make_tiles_from_spots(
             sdata,
             spots_key="spots",
             image_key="hne",
         )
-
-        # background is automatically classified
-        sdata.pl.render_shapes("spots_tiles", color="tile_classification", fill_alpha=0.5).pl.show()
 
     def test_plot_make_tiles_from_spots_with_tissue_mask_key(self):
         """Test make tiles from spots with tissue mask key on Visium H&E dataset."""
@@ -53,11 +47,9 @@ class TestMakeTilesFromSpots(PlotTester, metaclass=PlotTesterMeta):
             image_key="hne",
         )
 
+        # background is classified but preview plot doesn't have img
         sq.experimental.im.make_tiles_from_spots(
             sdata,
             spots_key="spots",
             tissue_mask_key="hne_tissue",
         )
-
-        # background is classified but preview plot doesn't have img
-        sdata.pl.render_shapes("spots_tiles", color="tile_classification", fill_alpha=0.5).pl.show()
