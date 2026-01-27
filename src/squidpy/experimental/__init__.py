e1327ba7c486128d25d7057d1cf7c2bd224411d9
Tim Treis <tim.treis@stud.uni-heidelberg.de>
2026-01-27 13:55:11 +0100

    Reorganize CellProfiler features into experimental.im submodule
    Moved calculate_image_features from experimental._feature to
    experimental.im._feature to follow the existing module structure.
    Now accessible as squidpy.experimental.im.calculate_image_features

--- src/squidpy/experimental/__init__.py
+++ src/squidpy/experimental/__init__.py
@@ -7,6 +7,5 @@ These features may change or be removed in future releases.
 from __future__ import annotations
 
 from . import im, pl
-from squidpy.experimental._feature import calculate_image_features
 
-__all__ = ["im", "pl", "calculate_image_features"]
+__all__ = ["im", "pl"]
