3b3df066687c89a025a9afc7b0ef6efd8b2feb78
Tim Treis <tim.treis@helmholtz-muenchen.de>
2026-06-01 10:04:20 -0400

    Stain normalization: Reinhard color transfer (#1191)

--- src/squidpy/experimental/im/_stain/__init__.py
+++ src/squidpy/experimental/im/_stain/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from squidpy.experimental.im._stain._constants import (
+    DEFAULT_LUMINOSITY_THRESHOLD,
     RUDERMAN_LAB_TO_LMS,
     RUDERMAN_LMS_TO_LAB,
     RUDERMAN_LMS_TO_RGB,
@@ -14,18 +15,35 @@ from squidpy.experimental.im._stain._conversion import (
     rgb_to_sda,
     sda_to_rgb,
 )
+from squidpy.experimental.im._stain._mask import luminosity_foreground_mask
+from squidpy.experimental.im._stain._normalize import (
+    apply_stain_normalization,
+    fit_stain_reference,
+)
 from squidpy.experimental.im._stain._reference import StainMethod, StainReference
+from squidpy.experimental.im._stain._reinhard import (
+    ReinhardParams,
+    apply_reinhard,
+    fit_reinhard,
+)
 
 __all__ = [
+    "DEFAULT_LUMINOSITY_THRESHOLD",
     "RUDERMAN_LAB_TO_LMS",
     "RUDERMAN_LMS_TO_LAB",
     "RUDERMAN_LMS_TO_RGB",
     "RUDERMAN_RGB_TO_LMS",
     "RUIFROK_HE",
     "SDA_SCALE",
+    "ReinhardParams",
     "StainMethod",
     "StainReference",
+    "apply_reinhard",
+    "apply_stain_normalization",
+    "fit_reinhard",
+    "fit_stain_reference",
     "lab_ruderman_to_rgb",
+    "luminosity_foreground_mask",
     "rgb_to_lab_ruderman",
     "rgb_to_sda",
     "sda_to_rgb",
