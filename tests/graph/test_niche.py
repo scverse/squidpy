099f9e8dc7767ceea8fe01abafd957713ecb63e1
selmanozleyen <syozleyen@gmail.com>
2026-08-25 19:19:32 +0200

    add import again resulting from the merge conflict

--- tests/graph/test_niche.py
+++ tests/graph/test_niche.py
@@ -8,7 +8,7 @@ from scipy.sparse import csr_matrix
 from spatialdata import SpatialData
 from spatialdata.models import TableModel
 
-from squidpy.gr import calculate_niche, calculate_niche_cellcharter, calculate_niche_neighborhood, spatial_neighbors_knn
+from squidpy.gr import calculate_niche, calculate_niche_cellcharter, calculate_niche_neighborhood, spatial_neighbors_knn, _niche
 
 N_NEIGHBORS = 20
 GROUPS = "celltype_mapped_refined"
